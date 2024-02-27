2a958c7b79c88e045fc8c2af855b132802054dd1
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-27 14:37:41 -0500

    Built User model

--- api/models/User.js
+++ api/models/User.js
@@ -1,9 +1,30 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true, minLength: 3, maxLength: 20 },
-  password: { type: String, required: true, minLength: 6, maxLength: 20 },
+	name: { firstName: { type: String, required: true, minLength: 2, maxLength: 50 },
+			lastName:  { type: String, required: true, minLength: 2, maxLength: 50 }},
+
+	contact: {  email:   {type: String, required: true, minLength: 4, maxLength: 50},
+				phone:   {type: String, required: true, minLength: 10, maxLength: 15},
+				address: { type: String, required: true, minLength: 2, maxLength: 50 },
+				city:    { type: String, required: true, minLength: 2, maxLength: 50 },
+				province:{ type: String, required: true, minLength: 2, maxLength: 50 }},
+
+	emergencyContact: { name:		 { firstName: { type: String, required: true, minLength: 2, maxLength: 50 },
+									   lastName:  { type: String, required: true, minLength: 2, maxLength: 50 }},
+						phone:		 {type: Number, required: true, minLength: 10, maxLength: 15},
+						relationship:{type: String, required: true, minLength: 2, maxLength: 50},},
+
+	garage: [ { year: { type: String, required: true, minLength: 4, maxLength: 4  },
+				make: { type: String, required: true, minLength: 2, maxLength: 50 },
+				model: {type: String, required: true, minLength: 2, maxLength: 50 }}],
+
+	group: { type: String, required: true, enum: ["green", "yellow", "red"] },
+	credits: { type: Number, required: true},
+				
+	password: { type: String, required: true, minLength: 8, maxLength: 50 },
 });
 
 // Export model
 module.exports = mongoose.model("User", UserSchema);
+
