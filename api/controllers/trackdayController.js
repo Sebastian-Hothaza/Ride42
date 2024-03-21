66c8575e0aa1e1d2c22d378ff0872daf8f91303a
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-03-21 18:48:01 -0400

    Added support for unregistering user from trackday

--- api/controllers/trackdayController.js
+++ api/controllers/trackdayController.js
@@ -5,6 +5,12 @@ const { body, validationResult } = require("express-validator");
 const jwt = require('jsonwebtoken')
 const ObjectId = require('mongoose').Types.ObjectId;
 
+/*
+A note about payments
+For now, all payments will be handled manually (credit card & e transfer.)
+API will feature support for mark paid & payWithCredit which will auto deduct credit where applicable
+*/
+
 
 // TODO: Can we move out validateForm and validateTrackdayID out to index for easier/more concise use? (Since userController also uses it)
 // TODO: Add method to mark user as paid
@@ -53,7 +59,7 @@ exports.register = [
         jwt.verify(req.cookies.JWT_TOKEN, process.env.JWT_CODE, asyncHandler(async (err, authData) => {
             if (err) return res.status(401).send({msg: 'JWT Validation Fail'});;
             // JWT is valid. Verify user is allowed to register for a trackday
-            if (authData.memberType === 'admin' || authData.id === req.params.userID){
+            if (authData.memberType === 'admin' || (authData.id === req.params.userID && 1)){
                 // Add user to trackday
                 const trackday = await Trackday.findById(req.params.trackdayID).exec();
                 trackday.members.push({
@@ -70,11 +76,33 @@ exports.register = [
     }
 ]
 
-exports.unregister = (req,res,next) => {
-    // Logged in user
-    // SENDS EMAIL NOTIFICATION
-    res.send('NOT YET IMPLEMENTED: unregister for user_id: '+req.params.userID+' at trackday: '+req.params.trackdayID)
-}
+// Removes a user from a trackday. Requires JWT with matching userID OR admin.
+// TODO: Check 7 day restriction + email
+exports.unregister = [
+    validateUserID,
+    validateTrackdayID,
+   
+    (req,res,next) => {
+        // Unbundle JWT and check if admin OR matching userID
+        jwt.verify(req.cookies.JWT_TOKEN, process.env.JWT_CODE, asyncHandler(async (err, authData) => {
+            if (err) return res.status(401).send({msg: 'JWT Validation Fail'});;
+            // JWT is valid. Verify user is allowed to register for a trackday
+            if (authData.memberType === 'admin' || (authData.id === req.params.userID && 1)){
+
+                const trackday = await Trackday.findById(req.params.trackdayID).exec();
+
+                // Check that the member we want to remove from the members array actually exists
+                const memberExists = trackday.members.some((member) => member.userID.equals(req.params.userID))
+                if (!memberExists) return res.status(404).send({msg: 'Member is not registered for that trackday'});
+
+                trackday.members = trackday.members.filter((member)=> !member.userID.equals(req.params.userID)) 
+                await trackday.save();
+                return res.sendStatus(200);
+            }
+            return res.sendStatus(401)
+        }))
+    }
+]
 
 exports.reschedule = (req,res,next) => {
     // Logged in user
