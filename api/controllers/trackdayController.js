91c2c79d955510c6fb7408650d938f4d017f8946
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-03-21 14:27:47 -0400

    Added support for trackday GET

--- api/controllers/trackdayController.js
+++ api/controllers/trackdayController.js
@@ -2,6 +2,9 @@ const Trackday = require('../models/Trackday');
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const jwt = require('jsonwebtoken')
+const ObjectId = require('mongoose').Types.ObjectId;
+
+// TODO: Can we move out validateForm and validateTrackdayID out to index for easier/more concise use? (Since userController also uses it)
 
 
 // Called by middleware functions
@@ -10,6 +13,15 @@ const jwt = require('jsonwebtoken')
 function validateForm(req,res,next){
     const errors = validationResult(req); // Extract the validation errors from a request.
     if (!errors.isEmpty())  return res.status(400).json(errors.mapped()); //TODO: How should this present errors? Update README, this may be the best decision
+    next();
+}
+
+// Called by middleware functions
+// Verify that the req.params.trackdayID is a valid objectID and that it exists in our DB
+async function validateTrackdayID(req, res, next){
+    if (!ObjectId.isValid(req.params.trackdayID)) return res.status(404).send({msg: 'trackdayID is not a valid ObjectID'});
+    const trackdayExists = await Trackday.exists({_id: req.params.trackdayID});
+    if (!trackdayExists) return res.status(404).send({msg: 'Trackday does not exist'});
     next();
 }
 
@@ -41,14 +53,34 @@ exports.checkin = (req,res,next) => {
 //////////////////////////////////////
 //              CRUD
 //////////////////////////////////////
-exports.trackday_get = (req,res,next) => {
-    //Admin only
-    res.send('NOT YET IMPLEMENTED: trackday_get for _id: '+req.params.trackdayID)
-}
 
+// Returns specific trackday. Requires JWT with admin.
+exports.trackday_get = [
+    validateTrackdayID,
+    (req,res,next) => {
+        jwt.verify(req.cookies.JWT_TOKEN, process.env.JWT_CODE, asyncHandler(async (err, authData) => {
+            if (err) return res.status(401).send({msg: 'JWT Validation Fail'});;
+            // JWT is valid. Verify user is admin and return the trackday
+            if (authData.memberType === 'admin'){
+                const trackday = await Trackday.findById(req.params.trackdayID).exec();
+                return res.status(200).json(trackday);
+            }
+            return res.sendStatus(401)
+        }))
+    }
+]
+
+// Returns all trackdays. Requires JWT with admin.
 exports.trackday_getALL = (req,res,next) => {
-    //Admin only
-    res.send('NOT YET IMPLEMENTED: trackday_getALL')
+    jwt.verify(req.cookies.JWT_TOKEN, process.env.JWT_CODE, asyncHandler(async (err, authData) => {
+        if (err) return res.status(401).send({msg: 'JWT Validation Fail'});;
+        // JWT is valid. Verify user is admin and return all trackdays
+        if (authData.memberType === 'admin'){
+            const trackdays = await Trackday.find().exec();
+            return res.status(200).json(trackdays);
+        }
+        return res.sendStatus(401)
+    }))
 }
 
 // Creates a trackday. Requires JWT with admin.
