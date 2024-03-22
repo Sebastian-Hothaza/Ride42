df80fece105925fa6fe948d5e359f90fdd69405b
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-03-22 19:08:57 -0400

    Added protection to mongoDB so it can not connect in test enviroment

--- api/mongoConfig.js
+++ api/mongoConfig.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 main().catch((err) => console.log(err));
 async function main() { 
-  await mongoose.connect(process.env.MONGODB_URI);
+  if (process.env.NODE_ENV !== 'test') await mongoose.connect(process.env.MONGODB_URI);
   if (process.env.NODE_ENV === 'development') console.log("DB Connection established");
 }
