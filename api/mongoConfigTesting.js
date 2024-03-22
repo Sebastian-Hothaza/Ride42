3336aa26a09766f9031f58bd3f093cee44e87430
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-03-22 19:09:23 -0400

    Added test server shut down clean up

--- api/mongoConfigTesting.js
+++ api/mongoConfigTesting.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 
+let mongoServer
+
 async function initializeMongoServer() {
-  const mongoServer = await MongoMemoryServer.create();
+  mongoServer = await MongoMemoryServer.create();
   const mongoUri = mongoServer.getUri();
 
   mongoose.connect(mongoUri);
@@ -14,10 +16,11 @@ async function initializeMongoServer() {
     }
     console.log(e);
   });
-
-  mongoose.connection.once("open", () => {
-    console.log(`MongoDB successfully connected to ${mongoUri}`);
-  });
 }
 
-module.exports = initializeMongoServer;
+async function takedownMongoServer() {
+  await mongoServer.stop();
+  await mongoose.disconnect();
+}
+
+module.exports = { initializeMongoServer, takedownMongoServer };
