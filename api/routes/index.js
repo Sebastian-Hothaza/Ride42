f7b50d6430feb18ca6bf4925dc827333a11e67c2
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-03-27 19:30:54 -0400

    Updated paths for new storage method of bikes

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,10 +6,10 @@ const trackdayController = require('../controllers/trackdayController')
 // Users
 router.post('/login', userController.login)
 router.get('/users/:userID/trackdays', userController.getTrackdays)
-router.get('/verify/:userID/:trackdayID', userController.verify)
+router.get('/verify/:userID/:trackdayID/:bikeID', userController.verify)
 router.put('/password/:userID', userController.updatePassword)
 router.post('/garage/:userID', userController.garage_post)
-router.delete('/garage/:userID', userController.garage_delete)
+router.delete('/garage/:userID/:bikeID', userController.garage_delete)
 
 
 router.get('/users/:userID', userController.user_get)
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'test') router.post('/admin', userController.admin)
 router.post('/register/:userID/:trackdayID', trackdayController.register) 
 router.delete('/register/:userID/:trackdayID', trackdayController.unregister)
 router.put('/register/:userID/:trackdayID_OLD/:trackdayID_NEW', trackdayController.reschedule) 
-router.post('/checkin/:userID/:trackdayID', trackdayController.checkin)
+router.post('/checkin/:userID/:trackdayID/:bikeID', trackdayController.checkin)
 router.get('/registrationNums/:trackdayID', trackdayController.getRegNumbers)
 
 
