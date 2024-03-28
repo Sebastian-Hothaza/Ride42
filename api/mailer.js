2c42bd018ab1659256f67520d4cc796acce648ab
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-03-28 14:45:20 -0400

    added support for arguments in email body

--- api/mailer.js
+++ api/mailer.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-// Usage example: if (process.env.NODE_ENV === 'production') await sendEmail("JohnDoe@gmail.com", "Hello from NodeMailer", mailTemplates.helloWorld)
+// Usage example: if (process.env.NODE_ENV === 'production') await sendEmail("JohnDoe@gmail.com", "Hello from NodeMailer", mailTemplates.helloWorld, {name: "Joe"})
 
 // Configure email sender info
 const transporter = nodemailer.createTransport({
@@ -12,8 +12,16 @@ const transporter = nodemailer.createTransport({
 	pass: process.env.ADMIN_EMAIL_PASSWORD,
   },
 });
+
 // Attempt to send the email
-async function main(recipient,subject,htmlBody){
+async function main(recipient,subject,htmlBody,args){
+    // Check if args were received, and if so, inject them into the HTML body
+    if (args && Object.keys(args).length){
+        Object.keys(args).forEach((arg)=>{
+            htmlBody = htmlBody.replace("{"+arg+"}", args[arg])
+        })
+    }
+    
     try{
         await transporter.sendMail({
             from: '"Ride42" <info@ride42.ca>', 
@@ -24,11 +32,10 @@ async function main(recipient,subject,htmlBody){
                 cid: 'sigImg' 
             }],
             subject: subject, 
-            html: htmlBody, 
+            html: htmlBody.replace("{name}", args.name), 
         });
     }catch(err){
         console.log(err)
     }
-
 }
 module.exports = main;
