03e71ec0cea18e3176f00f80e57a09dcf7f9ec40
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-03-28 14:45:06 -0400

    Added skeleton templates

--- api/mailer_templates.js
+++ api/mailer_templates.js
@@ -1,4 +1,199 @@
+/*
+    Manage email templates here
+    TODO: requestReview
+*/
+
+
 const helloWorld =
+`<body>
+    <div class="default-style">
+        Hello there {name}!
+    </div>
+    
+    <div class="io-ox-signature">
+        <div class="default-style">
+            <div class="default-style">
+                &nbsp;
+            </div>
+            <div>
+                <span style="color: #808080;">Sebastian Hothaza</span>
+            </div> <span style="color: #808080;"><em>Founder</em></span>
+        </div>
+        <div class="default-style">
+            <img class="aspect-ratio" style="max-width: 100%;" src="cid:sigImg" alt="" width="130" height="30">
+        </div>
+        <div class="default-style">
+            <a href="http://ride42.ca/"><strong>Visit us online</strong></a>
+        </div>
+        <div class="default-style">
+            <a href="https://www.facebook.com/groups/ride42/"><strong>Join us on Facebook</strong></a>
+        </div>
+    </div>
+</body>`
+
+const passwordChange =
+`<body>
+    <div class="default-style">
+        Hello there
+    </div>
+    
+    <div class="io-ox-signature">
+        <div class="default-style">
+            <div class="default-style">
+                &nbsp;
+            </div>
+            <div>
+                <span style="color: #808080;">Sebastian Hothaza</span>
+            </div> <span style="color: #808080;"><em>Founder</em></span>
+        </div>
+        <div class="default-style">
+            <img class="aspect-ratio" style="max-width: 100%;" src="cid:sigImg" alt="" width="130" height="30">
+        </div>
+        <div class="default-style">
+            <a href="http://ride42.ca/"><strong>Visit us online</strong></a>
+        </div>
+        <div class="default-style">
+            <a href="https://www.facebook.com/groups/ride42/"><strong>Join us on Facebook</strong></a>
+        </div>
+    </div>
+</body>`
+
+const registerTrackday =
+`<body>
+    <div class="default-style">
+        Hello there
+    </div>
+    
+    <div class="io-ox-signature">
+        <div class="default-style">
+            <div class="default-style">
+                &nbsp;
+            </div>
+            <div>
+                <span style="color: #808080;">Sebastian Hothaza</span>
+            </div> <span style="color: #808080;"><em>Founder</em></span>
+        </div>
+        <div class="default-style">
+            <img class="aspect-ratio" style="max-width: 100%;" src="cid:sigImg" alt="" width="130" height="30">
+        </div>
+        <div class="default-style">
+            <a href="http://ride42.ca/"><strong>Visit us online</strong></a>
+        </div>
+        <div class="default-style">
+            <a href="https://www.facebook.com/groups/ride42/"><strong>Join us on Facebook</strong></a>
+        </div>
+    </div>
+</body>`
+
+const unregisterTrackday =
+`<body>
+    <div class="default-style">
+        Hello there
+    </div>
+    
+    <div class="io-ox-signature">
+        <div class="default-style">
+            <div class="default-style">
+                &nbsp;
+            </div>
+            <div>
+                <span style="color: #808080;">Sebastian Hothaza</span>
+            </div> <span style="color: #808080;"><em>Founder</em></span>
+        </div>
+        <div class="default-style">
+            <img class="aspect-ratio" style="max-width: 100%;" src="cid:sigImg" alt="" width="130" height="30">
+        </div>
+        <div class="default-style">
+            <a href="http://ride42.ca/"><strong>Visit us online</strong></a>
+        </div>
+        <div class="default-style">
+            <a href="https://www.facebook.com/groups/ride42/"><strong>Join us on Facebook</strong></a>
+        </div>
+    </div>
+</body>`
+
+const rescheduleTrackday =
+`<body>
+    <div class="default-style">
+        Hello there
+    </div>
+    
+    <div class="io-ox-signature">
+        <div class="default-style">
+            <div class="default-style">
+                &nbsp;
+            </div>
+            <div>
+                <span style="color: #808080;">Sebastian Hothaza</span>
+            </div> <span style="color: #808080;"><em>Founder</em></span>
+        </div>
+        <div class="default-style">
+            <img class="aspect-ratio" style="max-width: 100%;" src="cid:sigImg" alt="" width="130" height="30">
+        </div>
+        <div class="default-style">
+            <a href="http://ride42.ca/"><strong>Visit us online</strong></a>
+        </div>
+        <div class="default-style">
+            <a href="https://www.facebook.com/groups/ride42/"><strong>Join us on Facebook</strong></a>
+        </div>
+    </div>
+</body>`
+
+const requestReview =
+`<body>
+    <div class="default-style">
+        Hello there
+    </div>
+    
+    <div class="io-ox-signature">
+        <div class="default-style">
+            <div class="default-style">
+                &nbsp;
+            </div>
+            <div>
+                <span style="color: #808080;">Sebastian Hothaza</span>
+            </div> <span style="color: #808080;"><em>Founder</em></span>
+        </div>
+        <div class="default-style">
+            <img class="aspect-ratio" style="max-width: 100%;" src="cid:sigImg" alt="" width="130" height="30">
+        </div>
+        <div class="default-style">
+            <a href="http://ride42.ca/"><strong>Visit us online</strong></a>
+        </div>
+        <div class="default-style">
+            <a href="https://www.facebook.com/groups/ride42/"><strong>Join us on Facebook</strong></a>
+        </div>
+    </div>
+</body>`
+
+const welcomeUser =
+`<body>
+    <div class="default-style">
+        Hello there
+    </div>
+    
+    <div class="io-ox-signature">
+        <div class="default-style">
+            <div class="default-style">
+                &nbsp;
+            </div>
+            <div>
+                <span style="color: #808080;">Sebastian Hothaza</span>
+            </div> <span style="color: #808080;"><em>Founder</em></span>
+        </div>
+        <div class="default-style">
+            <img class="aspect-ratio" style="max-width: 100%;" src="cid:sigImg" alt="" width="130" height="30">
+        </div>
+        <div class="default-style">
+            <a href="http://ride42.ca/"><strong>Visit us online</strong></a>
+        </div>
+        <div class="default-style">
+            <a href="https://www.facebook.com/groups/ride42/"><strong>Join us on Facebook</strong></a>
+        </div>
+    </div>
+</body>`
+
+const updateUser =
 `<body>
     <div class="default-style">
         Hello there
@@ -26,6 +221,4 @@ const helloWorld =
 </body>`
 
 
-
-
-module.exports = { helloWorld };
+module.exports = { helloWorld,passwordChange,registerTrackday,unregisterTrackday,rescheduleTrackday,requestReview,welcomeUser,updateUser };
