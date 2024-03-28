5c91db791563c1bcc62f1acdfc673d08d38aab2a
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-03-28 12:03:24 -0400

    Added path for presenting trackdays

--- api/routes/index.js
+++ api/routes/index.js
@@ -28,6 +28,8 @@ router.delete('/register/:userID/:trackdayID', trackdayController.unregister)
 router.put('/register/:userID/:trackdayID_OLD/:trackdayID_NEW', trackdayController.reschedule) 
 router.post('/checkin/:userID/:trackdayID/:bikeID', trackdayController.checkin)
 router.get('/registrationNums/:trackdayID', trackdayController.getRegNumbers)
+router.get('/presentTrackdays', trackdayController.presentTrackdays)
+
 
 
 router.get('/trackdays/:trackdayID', trackdayController.trackday_get)
