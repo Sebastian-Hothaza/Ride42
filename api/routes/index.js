eec9f3656304538bc41ce3cbab4d4cfbde9db3c8
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-03-29 19:56:16 -0400

    added presentTrackdays for user and removed getTrackdays

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,13 +5,11 @@ const trackdayController = require('../controllers/trackdayController')
 
 // Users
 router.post('/login', userController.login)
-router.get('/users/:userID/trackdays', userController.getTrackdays)
 router.get('/verify/:userID/:trackdayID/:bikeID', userController.verify)
 router.put('/password/:userID', userController.updatePassword)
 router.post('/garage/:userID', userController.garage_post)
 router.delete('/garage/:userID/:bikeID', userController.garage_delete)
 router.get('/qrcode/:userID/:bikeID', userController.requestQRCode)
-
 
 
 router.get('/users/:userID', userController.user_get)
@@ -24,13 +22,12 @@ router.delete('/users/:userID', userController.user_delete)
 if (process.env.NODE_ENV === 'test') router.post('/admin', userController.admin)
 
 // Trackdays
-
 router.post('/register/:userID/:trackdayID', trackdayController.register) 
 router.delete('/register/:userID/:trackdayID', trackdayController.unregister)
 router.put('/register/:userID/:trackdayID_OLD/:trackdayID_NEW', trackdayController.reschedule) 
 router.post('/checkin/:userID/:trackdayID/:bikeID', trackdayController.checkin)
 router.get('/presentTrackdays', trackdayController.presentTrackdays)
-
+router.get('/presentTrackdays/:userID', trackdayController.presentTrackdaysForUser)
 
 
 router.get('/trackdays/:trackdayID', trackdayController.trackday_get)
