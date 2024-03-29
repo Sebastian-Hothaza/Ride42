b57d5ee947427d4f835165e809579d2d10c4a306
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-03-28 22:46:25 -0400

    Added template for QR code request

--- api/mailer_templates.js
+++ api/mailer_templates.js
@@ -240,5 +240,33 @@ const updateUser =
     </div>
 </body>`
 
+const QRCodeRequest =
+`<body>
+    <div class="default-style">
+        A QR code has been requested for {name}(userID: {userID}) for bikeID: {bikeID}
+    </div>
+    
+    <div class="io-ox-signature">
+        <div class="default-style">
+            <div class="default-style">
+                &nbsp;
+            </div>
+            <div>
+                <span style="color: #808080;">Sebastian Hothaza</span>
+            </div> <span style="color: #808080;"><em>Founder</em></span>
+        </div>
+        <div class="default-style">
+            <img class="aspect-ratio" style="max-width: 100%;" src="cid:sigImg" alt="" width="130" height="30">
+        </div>
+        <div class="default-style">
+            <a href="http://ride42.ca/"><strong>Visit us online</strong></a>
+        </div>
+        <div class="default-style">
+            <a href="https://www.facebook.com/groups/ride42/"><strong>Join us on Facebook</strong></a>
+        </div>
+    </div>
+</body>`
 
-module.exports = { helloWorld,passwordChange,registerTrackday,unregisterTrackday,unregisterTrackday_admin, rescheduleTrackday,requestReview,welcomeUser,updateUser };
+
+module.exports = {  helloWorld,passwordChange,registerTrackday,unregisterTrackday,unregisterTrackday_admin,
+                    rescheduleTrackday,requestReview,welcomeUser,updateUser, QRCodeRequest };
