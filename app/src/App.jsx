e6059fdfd2a3f4ced06219b68a5ea07f84cebc36
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-01 18:51:12 -0400

    Added API fetching and dates raw data display

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,32 @@
 import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from "react";
 
 import './App.css'
 
+ 
+
 function App() {
+    const [allTrackdays, setAllTrackdays] = useState('');
+    const APIServer = (process.env.NODE_ENV === 'production')? 'https://api.ride42.ca/':'http://localhost:3000/'
+    async function fetchAPIData(){
+        try{
+            const response = await fetch(APIServer+'presentTrackdays');
+            if (!response.ok) throw new Error("Failed to get API Data")
+            const data = await response.json();
+            setAllTrackdays(data);
+        }catch(err){
+            console.log(err.message)
+        }
+    }
+
+    useEffect(() => {
+        fetchAPIData();
+    }, [])
+
 	return (
-		<Outlet />
+		<>
+		    <Outlet context={allTrackdays}/>
+		</>
 	)
 }
 
--- app/src/pages/Dates.jsx
+++ app/src/pages/Dates.jsx
@@ -1,9 +1,17 @@
-const Dates = () => {
-    return (
-      <div>
-        <h1>Dates</h1>
-      </div>
-    );
-  };
-  
-  export default Dates;
+import { useOutletContext } from "react-router-dom";
+import { useState } from "react";
+
+function Dates(){
+	const allTrackdays = useOutletContext();
+
+	return (
+		<div>
+			<h1>Dates</h1>
+			{allTrackdays && allTrackdays.map((item)=>{
+				return <li key={item.id}>{item.date}</li>
+			})}
+		</div>
+	);
+};
+	
+	export default Dates;
