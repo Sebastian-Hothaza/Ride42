de8d80ad98590031062f56dd66fa72cdfead1d8f
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-03 19:06:35 -0400

    added filler content

--- app/src/pages/Dates.jsx
+++ app/src/pages/Dates.jsx
@@ -2,20 +2,33 @@ import { useOutletContext } from "react-router-dom";
 import { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Card from "../components/Card"
 
 import './stylesheets/dates.css'
 
+import square from '../assets/square.jpg'
+
 function Dates() {
 	const allTrackdays = useOutletContext();
+	const datesArray = []
+	if (allTrackdays){
+		allTrackdays.map((trackday)=>{
+			datesArray.push(trackday.date)
+		})
+	}
 
+	const mockText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices vitae auctor eu augue ut. Maecenas accumsan lacus vel facilisis. Pharetra diam sit amet nisl suscipit. Nibh ipsum consequat nisl vel pretium lectus quam id. Enim lobortis scelerisque fermentum dui faucibus in ornare. Facilisi nullam vehicula ipsum a arcu cursus vitae congue. In ornare quam viverra orci sagittis eu volutpat.'
+
+	
 	return (
 		<>
 			<Navbar />
 			<div className="main">
-				{allTrackdays && allTrackdays.map((item) => {
-					return <h2 key={item.id}>{item.date}</h2>
-				})}
+				<Card heading='Dates' body={datesArray[0]+'   '+datesArray[1]} img={square} inverted={false}/>
+				<Card heading='Schedule' body={mockText} img={square} inverted={true}/>
+				<Card heading='Pricing Info' body={mockText} img={square} inverted={false}/>
 			</div>
+			
 
 			<Footer />
 		</>
--- app/src/pages/Rules.jsx
+++ app/src/pages/Rules.jsx
@@ -1,12 +1,21 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+import Card from "../components/Card"
+
+import square from '../assets/square.jpg'
+
 const Rules = () => {
+	const mockText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices vitae auctor eu augue ut. Maecenas accumsan lacus vel facilisis. Pharetra diam sit amet nisl suscipit. Nibh ipsum consequat nisl vel pretium lectus quam id. Enim lobortis scelerisque fermentum dui faucibus in ornare. Facilisi nullam vehicula ipsum a arcu cursus vitae congue. In ornare quam viverra orci sagittis eu volutpat.'
+
+
 	return (
 		<div>
 			<Navbar />
 			<div className="main">
-				<h1>Rules</h1>
+				<Card heading='Your Bike' body={mockText} img={square} inverted={false}/>
+				<Card heading='Your Gear' body={mockText} img={square} inverted={true}/>
+				<Card heading='Our Policies' body={mockText} img={square} inverted={false}/>
 			</div>
 			<Footer />
 		</div>
