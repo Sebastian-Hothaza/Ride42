8fb8a9e81ead87bd6088dbc28d3edda385f71568
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-05 15:47:54 -0400

    Added shop page

--- app/src/pages/Shop.jsx
+++ app/src/pages/Shop.jsx
@@ -1,12 +1,27 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+import './stylesheets/shop.css'
+
+import square from '../assets/square.jpg'
+
 const Shop = () => {
 	return (
 		<div>
 			<Navbar />
-			<div className="main">
-				<h1>Shop</h1>
+			<div className="main inverted">
+				<a href="http://google.ca" target="_blank" className="shopCard">
+					<img src={square} alt="Pirelli Tire Photo" />
+					<div>Tires</div>
+				</a>
+				<a href="http://google.ca" target="_blank" className="shopCard">
+					<img src={square} alt="PLUS Racing Gear Suit Photo" />
+					<div>Gear</div>
+				</a>
+				<a href="http://google.ca" target="_blank" className="shopCard">
+					<img src={square} alt="KYT Helmet Photo" />
+					<div>Helmets</div>
+				</a>
 			</div>
 			<Footer />
 		</div>
