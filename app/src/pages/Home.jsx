e1fc37dc30c2fed2ed25f212354a0fe816cbbee9
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-04 20:07:39 -0400

    Added our partners section to home page

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -6,6 +6,9 @@ import Card from "../components/Card"
 import pageContent from './Pagecontent'
 import square from '../assets/square.jpg'
 
+import raceway from '../assets/raceway.png'
+import partners from '../assets/partners.png'
+
 import './stylesheets/home.css'
 
 
@@ -16,17 +19,22 @@ const Home = () => {
 
 	return (
 		<>
-		<Navbar />
-		<div className="heroImage">
-			<div className="heroText">
-				{/* TODO: Add some hero text content here */}
+			<Navbar />
+			<div className="heroImage">
+				<div className="heroText">
+					{/* TODO: Add some hero text content here */}
+				</div>
 			</div>
-		</div>
-		<div className="main" style={{padding: '2rem 0'}}>
-			<Card heading='About Us' body={pageContent.HTML_AboutUs} img={square} inverted={false}/>
-			<Card heading='What We Offer' body={pageContent.HTML_Offer} img={square} inverted={true}/>
-		</div>
-		<Footer />
+			<div className="main" style={{ padding: '2rem 0' }}>
+				<Card heading='About Us' body={pageContent.HTML_AboutUs} img={square} inverted={false} />
+				<Card heading='What We Offer' body={pageContent.HTML_Offer} img={square} inverted={true} />
+				<div className="imageContainer">
+					<img src={partners} alt="Picture of our partners" />
+					<img src={raceway} alt="Picture of Grand Bend Track" />
+				</div>
+
+			</div>
+			<Footer />
 		</>
 	);
 };
--- app/src/pages/Pagecontent.jsx
+++ app/src/pages/Pagecontent.jsx
@@ -4,28 +4,41 @@
 
 
 const HTML_AboutUs = <p>
-Founded in 2024, Ride42 primarily focuses on running motorcycle trackdays at Grand Bend Motorplex. Many riders begin their motorcycle journey on the street
-and soon realize the danger of exploring the limits of their machine on public roads. With other road users, unknown road surface conditions and no run off
-area, the consequences and risks simply outweigh the rewards.<br></br>
-<br></br>
-Motorcycle trackdays allow enthusiasts a safe environment where they can truly enjoy and explore the limits of their machines. If you own a modern supersport
-motorcycle and have not been on track with it, you are doing it a disservice! Trackdays allow you to improve your skills in a safe and structured environment;
-skills which will directly translate to making you a more competent and safe rider.<br></br>
-<br></br>
-At Ride42, we have 3 core principles. First, we want to foster an environment where new riders feel welcome and at home. We are centered around building
-relationships within the community and creating long lasting memories. Second, we want to lower the barrier of entry for the sport and make it accessible.
-It’s no secret that motorsports are not a cheap hobby, however, we aim to mitigate that by providing competitive pricing and excellent value. Lastly, we want
-to provide our members with the right tools to continually develop their skills. This will be done through our coaching system and carefully selected
-partnerships with organizations that share our values and can benefit our community. <br></br>
-<br></br>
-We hope to see you on track soon!
+    Founded in 2024, Ride42 primarily focuses on running motorcycle trackdays at Grand Bend Motorplex. Many riders begin their motorcycle journey on the street
+    and soon realize the danger of exploring the limits of their machine on public roads. With other road users, unknown road surface conditions and no run off
+    area, the consequences and risks simply outweigh the rewards.<br></br>
+    <br></br>
+    Motorcycle trackdays allow enthusiasts a safe environment where they can truly enjoy and explore the limits of their machines. If you own a modern supersport
+    motorcycle and have not been on track with it, you are doing it a disservice! Trackdays allow you to improve your skills in a safe and structured environment;
+    skills which will directly translate to making you a more competent and safe rider.<br></br>
+    <br></br>
+    At Ride42, we have 3 core principles. First, we want to foster an environment where new riders feel welcome and at home. We are centered around building
+    relationships within the community and creating long lasting memories. Second, we want to lower the barrier of entry for the sport and make it accessible.
+    It’s no secret that motorsports are not a cheap hobby, however, we aim to mitigate that by providing competitive pricing and excellent value. Lastly, we want
+    to provide our members with the right tools to continually develop their skills. This will be done through our coaching system and carefully selected
+    partnerships with organizations that share our values and can benefit our community. <br></br>
+    <br></br>
+    We strive to provide the best trackday experience in southwestern Ontario and hope to see you on track soon!
 </p>
 
 
-const HTML_Offer =<p>
-We offer stuff
-</p>
+const HTML_Offer = <div>
+    <p>
+        Hosted exclusively at Grand Bend Motorplex, our goal is to provide you a consistent trackday experience where you can expect:
+        <br></br><br></br>
+    </p>
+    <ul className="withBullets">
+        <li>Trackday run by experienced personnel who are eager to ensure you have a great time</li>
+        <li>Safe and structured riding divided into groups based on skill level. Occasionally we may go down to 2 groups; our goal is to maximize your time on track while maintaining a safe environment</li>
+        <li>Experienced pro level regional and national racers available for one-on-one coaching and guidance</li>
+        <li>BBQ lunch with refreshments</li>
+        <li>Paved, well maintained track environment without the hazards, limitations or distractions you’d typically find on the street</li>
+        <li>Professional photographs from an on-site photographer</li>
+    </ul>
+</div>
 
 
 
-export default { HTML_AboutUs,  HTML_Offer}
+
+
+export default { HTML_AboutUs, HTML_Offer }
