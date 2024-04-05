c7b0d7c7477d945c14fc925f486be00581e95e61
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-05 15:15:51 -0400

    Update FAQ page

--- app/src/pages/Faq.jsx
+++ app/src/pages/Faq.jsx
@@ -1,13 +1,15 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card"
+import './stylesheets/faq.css'
+import pageContent from './Pagecontent'
 
 const Faq = () => {
 	return (
 		<div>
 			<Navbar />
 			<div className="main">
-				<h1>FAQ</h1>
+				<Card heading='FAQ' body={pageContent.HTML_Faq} inverted={false} />
 			</div>
 			<Footer />
 		</div>
