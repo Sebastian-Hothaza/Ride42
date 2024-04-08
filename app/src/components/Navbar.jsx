043ae43aa9fb37c299f068ebfa3daea1648fa609
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-08 13:19:46 -0400

    Updated to use css modules

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -5,46 +5,47 @@ import r42 from '../assets/ride42.png'
 import r42_small from '../assets/r42_small.png'
 import helmet from '../assets/helmet.png'
 
-import './stylesheets/navbar.css'
+import styles from './stylesheets/Navbar.module.css'
 
 const Navbar = () => {
 	const [expandedMenu, setExpandedMenu] = useState(false)
 
 	return (
-		<nav className="navbar">
+		<>
+		<nav className={styles.navbar}>
 			{/* DESKTOP */}
-			<ul className="navbar-main">
-				<Link to="/"><img src={r42} id='headerImg'></img></Link>
+			<ul className={styles.navbarMain}>
+				<Link to="/"><img src={r42} id={styles.headerImg}></img></Link>
 				<NavLink to="/dates">Dates</NavLink>
 				<NavLink to="/rules">Rules</NavLink>
 				<NavLink to="/faq">FAQ</NavLink>
 				<NavLink to="/shop">Shop</NavLink>
 			</ul>
-			<div className="navbar-dashboard">
-				<NavLink to="/dashboard"><img src={helmet} id='helmetImg'></img></NavLink>
+			<div className={styles.navbarDashboard}>
+				<NavLink to="/dashboard"><img src={helmet} id={styles.helmetImg}></img></NavLink>
 			</div>
-
 			{/* MOBILE */}
-			<ul className="navbar-main-mobile">
-				<li className="menu-button" onClick={() => setExpandedMenu(!expandedMenu)}>
-					<span className="bar"></span>
-					<span className="bar"></span>
-					<span className="bar"></span>
+			<ul className={styles.navbarMainMobile}>
+				<li className={styles.menuButton} onClick={() => setExpandedMenu(!expandedMenu)}>
+					<span className={styles.bar}></span>
+					<span className={styles.bar}></span>
+					<span className={styles.bar}></span>
 				</li>
-				<Link to="/"><img src={r42_small} id='headerImg'></img></Link>
-				<Link to="/dashboard" className="navbar-dashboard-mobile"><img src={helmet} id='helmetImg'></img></Link>
+				<Link to="/"><img src={r42_small} id={styles.headerImg}></img></Link>
+				<Link to="/dashboard" className={styles.navbarDashboardMobile}><img src={helmet} id={styles.helmetImg}></img></Link>
 			</ul>
-			{expandedMenu &&
-				<ul className="menu-button-links" >
-					<Link reloadDocument to="/">Home</Link>
-					<Link reloadDocument to="/dates">Dates</Link>
-					<Link reloadDocument to="/rules">Rules</Link>
-					<Link reloadDocument to="/faq">FAQ</Link>
-					<Link reloadDocument to="/shop">Shop</Link>
-				</ul>
-			}
-
+			
 		</nav>
+		{expandedMenu &&
+			<ul className={styles.menuButtonLinks} >
+				<Link reloadDocument to="/">Home</Link>
+				<Link reloadDocument to="/dates">Dates</Link>
+				<Link reloadDocument to="/rules">Rules</Link>
+				<Link reloadDocument to="/faq">FAQ</Link>
+				<Link reloadDocument to="/shop">Shop</Link>
+			</ul>
+		}
+		</>
 	);
 };
 
