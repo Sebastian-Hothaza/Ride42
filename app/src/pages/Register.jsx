6647e6764b22cddfbe1ce077689bc28ffd019899
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-10 15:20:44 -0400

    Added client side form validation for registering new user

--- app/src/pages/Register.jsx
+++ app/src/pages/Register.jsx
@@ -1,13 +1,13 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import { useOutletContext, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 
 import styles from './stylesheets/Register.module.css'
 
 import Card from "../components/Card"
 
 const Register = () => {
-
+	const [registerErrors, setRegisterErrors] = useState();
 	const { APIServer } = useOutletContext();
 	const navigate = useNavigate();
 
@@ -16,17 +16,17 @@ const Register = () => {
 			<div className={styles.inputSection}>
 				<div className={styles.inputPairing}>
 					<label htmlFor="firstName">First Name:</label>
-					<input type="text" id="firstName" name="firstName"></input>
+					<input type="text" id="firstName" name="firstName" required minLength={2} maxLength={50}></input>
 				</div>
 
 				<div className={styles.inputPairing}>
 					<label htmlFor="lastName">Last Name:</label>
-					<input type="text" id="lastName" name="lastName"></input>
+					<input type="text" id="lastName" name="lastName"  required minLength={2} maxLength={50}></input>
 				</div>
 
 				<div className={styles.inputPairing}>
 					<label htmlFor="group">Group:</label>
-					<select name="group" id="group" form={styles.registerForm}>
+					<select name="group" id="group" form={styles.registerForm} required>
 						<option key="groupNone" value="">---Choose Group---</option>
 						<option key="green" value="green">Green (Novice)</option>
 						<option key="yellow" value="yellow">Yellow (Intermediate)</option>
@@ -39,27 +39,27 @@ const Register = () => {
 			<div className={styles.inputSection}>
 				<div className={styles.inputPairing}>
 					<label htmlFor="email">Email:</label>
-					<input type="email" id="email" name="email"></input>
+					<input type="email" id="email" name="email" required></input>
 				</div>
 				<div className={styles.inputPairing}>
 					<label htmlFor="emailConfirm">Confirm Email:</label>
-					<input type="text" id="emailConfirm" name="emailConfirm"></input>
+					<input type="email" id="emailConfirm" name="emailConfirm" required onInput={checkEmailMatches}></input>
 				</div>
 				<div className={styles.inputPairing}>
 					<label htmlFor="phone">Phone:</label>
-					<input type="tel" id="phone" name="phone"></input>
+					<input type="tel" id="phone" name="phone" required minLength={10} maxLength={10}></input>
 				</div>
 				<div className={styles.inputPairing}>
 					<label htmlFor="address">Street Address:</label>
-					<input type="text" id="address" name="address"></input>
+					<input type="text" id="address" name="address" required minLength={2} maxLength={50}></input>
 				</div>
 				<div className={styles.inputPairing}>
 					<label htmlFor="city">City:</label>
-					<input type="text" id="city" name="city"></input>
+					<input type="text" id="city" name="city" required minLength={2} maxLength={50}></input>
 				</div>
 				<div className={styles.inputPairing}>
 					<label htmlFor="province">Province:</label>
-					<select name="province" id="province" form={styles.registerForm}>
+					<select name="province" id="province" form={styles.registerForm} required>
 						<option key="ontario" value="ontario">Ontario</option>
 						<option key="quebec" value="quebec">Quebec</option>
 						<option key="other" value="other">Other</option>
@@ -69,33 +69,40 @@ const Register = () => {
 			<div className={styles.inputSection}>
 				<div className={styles.inputPairing}>
 					<label htmlFor="EmergencyName_firstName">Emergency Contact - First Name:</label>
-					<input type="text" id="EmergencyName_firstName" name="EmergencyName_firstName"></input>
+					<input type="text" id="EmergencyName_firstName" name="EmergencyName_firstName" required minLength={2} maxLength={50}></input>
 				</div>
 				<div className={styles.inputPairing}>
 					<label htmlFor="EmergencyName_lastName">Emergency Contact - Last Name:</label>
-					<input type="text" id="EmergencyName_lastName" name="EmergencyName_lastName"></input>
+					<input type="text" id="EmergencyName_lastName" name="EmergencyName_lastName" required minLength={2} maxLength={50}></input>
 				</div>
 				<div className={styles.inputPairing}>
 					<label htmlFor="EmergencyPhone">Emergency Contact - Phone:</label>
-					<input type="text" id="EmergencyPhone" name="EmergencyPhone"></input>
+					<input type="tel" id="EmergencyPhone" name="EmergencyPhone" required minLength={10} maxLength={10}></input>
 				</div>
 				<div className={styles.inputPairing}>
 					<label htmlFor="EmergencyRelationship">Emergency Contact - Relationship:</label>
-					<input type="text" id="EmergencyRelationship" name="EmergencyRelationship"></input>
+					<input type="text" id="EmergencyRelationship" name="EmergencyRelationship" required minLength={2} maxLength={50}></input>
 				</div>
 
 			</div>
+
 			<div className={styles.inputSection}>
 				<div className={styles.inputPairing}>
 					<label htmlFor="password">Password:</label>
-					<input type="password" id="password" name="password"></input>
+					<input type="password" id="password" name="password" required onInput={checkPswFormat}></input>
 				</div>
 				<div className={styles.inputPairing}>
 					<label htmlFor="passwordConfirm">Confirm Password:</label>
-					<input type="password" id="passwordConfirm" name="passwordConfirm"></input>
+					<input type="password" id="passwordConfirm" name="passwordConfirm" required onInput={checkPswMatches}></input>
 				</div>
 			</div>
 
+			{
+				registerErrors && registerErrors.length > 0 &&
+				<ul className="errorText">Encountered the following errors:
+					{registerErrors.map((errorItem) => <li key={errorItem}>{errorItem}</li>)}
+				</ul>
+			}
 
 			<button className={styles.registerBtn} type="submit">Submit Registration</button>
 
@@ -104,25 +111,61 @@ const Register = () => {
 		</form >
 
 
-	async function handleRegisterSubmit(e) {
-		e.preventDefault();
-		const formData = new FormData(e.target);
-		const response = await fetch(APIServer + 'users/', {
-			method: 'POST',
-			headers: {
-				'Content-type': 'application/json; charset=UTF-8',
-			},
-			body: JSON.stringify(Object.fromEntries(formData))
-		})
-		if (response.status === 201) {
-			navigate("/dashboard");
-		} else if (response.status === 400) {
-			const data = await response.json();
-			console.log(data)
+
+	function checkEmailMatches() {
+		let input = document.getElementById('emailConfirm');
+		if (input.value == document.getElementById('email').value) {
+			input.setCustomValidity(''); // input is valid -- reset the error message
 		} else {
-			setEditUserInfoErrors(['API Error'])
+			input.setCustomValidity('Email must match');
 		}
 	}
+	function checkPswFormat() {
+		let input = document.getElementById('password');
+		if ((/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/).test(input.value) && input.value.length>=8 && input.value.length<=50) {
+			input.setCustomValidity(''); // input is valid -- reset the error message
+		} else {
+			input.setCustomValidity('Password must contain minimum 8 characters and be a combination of letters and numbers');
+		}
+	}
+	function checkPswMatches() {
+		let input = document.getElementById('passwordConfirm');
+		if (input.value == document.getElementById('password').value) {
+			input.setCustomValidity(''); // input is valid -- reset the error message
+		} else {
+			input.setCustomValidity('Password must match');
+		}
+	}
+
+
+	async function handleRegisterSubmit(e) {
+		e.preventDefault();
+		
+		const formData = new FormData(e.target);
+		try {
+			const response = await fetch(APIServer + 'users/', {
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json; charset=UTF-8',
+				},
+				body: JSON.stringify(Object.fromEntries(formData))
+			})
+			if (response.ok) {
+				navigate("/dashboard");
+			} else if (response.status === 400) {
+				const data = await response.json();
+				setRegisterErrors(data.msg);
+			} else if (response.status === 409) {
+				const data = await response.json();
+				setRegisterErrors([data.msg]);
+			} else {
+				throw new Error('API Failure')
+			}
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
 
 
 	return (
