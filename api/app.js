7029eff0cf5641747fde2eff0248cf229fec4a21
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-10 19:49:47 -0400

    Added feature to simulate slow network for testing

--- api/app.js
+++ api/app.js
@@ -4,15 +4,28 @@ require("./mongoConfig")
 
 const app = express();
 
+// Simulate slow network
+const simulateSlowNetowrk = false;
+if (simulateSlowNetowrk) {
+  const sleep = (ms) => new Promise(
+    resolve => setTimeout(resolve, ms));
+  app.use(async (req, res, next) => {
+    console.log('Simulating slow server')
+    await sleep(4000)
+    next();
+  })
+}
+
+
 // cors setup
 const cors = require('cors')
 
 app.use(cors({
-  origin: (process.env.NODE_ENV === 'production') ? "https://ride42.netlify.app" : "http://localhost:5173",  credentials: true,
+  origin: process.env.NODE_ENV === 'production' ? "https://ride42.netlify.app" : "http://localhost:5173", credentials: true,
 }));
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const cookieParser = require('cookie-parser')
@@ -25,19 +38,19 @@ app.use('/', router);
 
 // catch 404 and forward to error handler
 const createError = require('http-errors');
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') !== 'production' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json({"msg":"BAD REQUEST"});
+  res.json({ "msg": "BAD REQUEST" });
 });
 
 module.exports = app;
