0b3a0531a68f2be2a591777c99671b3df8e3a380
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-13 15:53:55 -0400

    Padding and spacing changes

--- app/src/pages/Shop.jsx
+++ app/src/pages/Shop.jsx
@@ -6,21 +6,21 @@ import kyt from '../assets/kyt.png'
 
 const Shop = () => {
 	return (
-		<div className="content">
-			<div className={styles.invertedContent}>
-				<a href="https://docs.google.com/forms/d/e/1FAIpQLScMWJdjsgDFigjbJutJtFg7zzrUBpboBkEmTTWQEkjJyWtPPg/viewform?usp=sf_link" target="_blank" className={styles.shopCard}>
-					<img src={pirelli} alt="Pirelli Tire Photo" />
-					<div>Tires</div>
-				</a>
-				<a href="https://docs.google.com/forms/d/e/1FAIpQLScGL_Dlr5OdkSkfqasaE85daJwk4oNZ_lyWVmWqHjWWEElHiQ/viewform?usp=sf_link" target="_blank" className={styles.shopCard}>
-					<img src={plus} alt="PLUS Racing Gear Suit Photo" />
-					<div>Gear</div>
-				</a>
-				<a href="https://www.eurorace.ca/kyt" target="_blank" className={styles.shopCard}>
-					<img src={kyt} alt="KYT Helmet Photo" />
-					<div>Helmets</div>
-				</a>
-			</div>
+		<div className={styles.content}>
+
+			<a href="https://docs.google.com/forms/d/e/1FAIpQLScMWJdjsgDFigjbJutJtFg7zzrUBpboBkEmTTWQEkjJyWtPPg/viewform?usp=sf_link" target="_blank" className={styles.shopCard}>
+				<img src={pirelli} alt="Pirelli Tire Photo" />
+				<div>Tires</div>
+			</a>
+			<a href="https://docs.google.com/forms/d/e/1FAIpQLScGL_Dlr5OdkSkfqasaE85daJwk4oNZ_lyWVmWqHjWWEElHiQ/viewform?usp=sf_link" target="_blank" className={styles.shopCard}>
+				<img src={plus} alt="PLUS Racing Gear Suit Photo" />
+				<div>Gear</div>
+			</a>
+			<a href="https://www.eurorace.ca/kyt" target="_blank" className={styles.shopCard}>
+				<img src={kyt} alt="KYT Helmet Photo" />
+				<div>Helmets</div>
+			</a>
+
 
 		</div>
 	);
