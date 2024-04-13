b9f8ad288c6a11e2c59f544d9ed98bbef2e07311
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-13 17:51:29 -0400

    Added notification modal

--- app/src/components/Modal.jsx
+++ app/src/components/Modal.jsx
@@ -1,7 +1,9 @@
 import styles from './stylesheets/Modal.module.css'
 
+import checkmark from './../assets/checkmark.png'
+
 // open: boolean to decide if modal should be open
-// type: oneOf{loading, confirmation, select}
+// type: oneOf{loading, confirmation, select, notification}
 // text: Text content of the modal
 // onClose: Modal calls this function when closed
 // onOK: Modal calls this function when OK button is pressed
@@ -12,7 +14,10 @@ import styles from './stylesheets/Modal.module.css'
 
 
 
-const Modal = ({ open, type, text, onClose, onOK, okText, closeText,  }) => {
+const Modal = ({ open, type, text, onClose, onOK, okText, closeText, }) => {
+
+
+
     if (!open) return null
 
     switch (type) {
@@ -34,6 +39,17 @@ const Modal = ({ open, type, text, onClose, onOK, okText, closeText,  }) => {
                         {text}
                         {okText && <button className={`actionButton ${styles.confirmBtn}`} onClick={onOK}>{okText}</button>}
                         <button className='actionButton' onClick={onClose}>{closeText}</button>
+                    </div>
+                </>
+            );
+        case 'notification':
+            setTimeout(() => onClose(), 1500)
+            return (
+                <>
+                    <div className={styles.overlay} ></div>
+                    <div id={styles.modalCheckmark} className={styles.modal}>
+                        <img id={styles.modalCheckmarkIMG} src={checkmark} alt="checkmark icon" />
+                        {text}
                     </div>
                 </>
             );
