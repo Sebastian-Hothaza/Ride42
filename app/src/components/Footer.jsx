a6b60250c6e08573525d0fa307aa730369b3ae34
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-13 22:14:50 -0400

    Added social media footer links

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -2,6 +2,8 @@ import { Link, NavLink } from "react-router-dom";
 import { useState } from 'react'
 
 import r42_small from '../assets/r42_small.png'
+import fb from '../assets/fb.webp'
+import insta from '../assets/insta.webp'
 
 import styles from './stylesheets/Footer.module.css'
 
@@ -9,13 +11,25 @@ const Navbar = () => {
 
       return (
             <div className={styles.footer}>
-                  <div id={styles.desktopContact}>Contact Us:<br></br> <a href="mailto:info@ride42.ca">info@ride42.ca</a></div>
-                  <div id={styles.mobileContact}><a href="mailto:info@ride42.ca"><span className='material-symbols-outlined'>email</span></a></div>
+                  <div className={styles.leftItems}>
+                        <div id={styles.desktopContact}>Contact Us:<br></br> <a href="mailto:info@ride42.ca">info@ride42.ca</a></div>
+                        <div id={styles.mobileContact}><a href="mailto:info@ride42.ca"><span className={`${styles.mobileToolbarIcons} material-symbols-outlined`}>email</span></a></div>
+                        <a href="https://www.facebook.com/groups/ride42"><img src={fb} className={styles.footerIcon}></img></a>
+                        <a href="https://www.instagram.com/ride42_official"><img src={insta} className={styles.footerIcon}></img></a>
+                  </div>
+
+
+
+
                   <Link to="/" id={styles.footerLogo}><img src={r42_small} id={styles.headerImg}></img></Link>
-                  <div>© Sebastian Hothaza</div>
+
+
+                  <div id={styles.copyright}>© Sebastian<br></br>Hothaza</div>
             </div>
       );
 };
 
 export default Navbar;
 
+
+//className={`${styles.mobileToolbarIcons} material-symbols-outlined ${activeTab == 'profile' ? styles.selected : undefined}`}> person </span>
