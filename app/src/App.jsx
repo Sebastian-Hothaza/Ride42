bf8c34d41571f316f278f0bb1c051fe4fa716e1b
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-18 09:59:09 -0400

    severed connection to API

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 
 
 function App() {
-    const APIServer = (process.env.NODE_ENV === 'production') ? 'https://api.ride42.ca/' : 'http://localhost:3000/'
+    const APIServer = (process.env.NODE_ENV === 'production') ? 'https://api.ZZZZZride42ZZZZ.ca/' : 'http://localhost:3000/'
     const [loggedIn, setLoggedIn] = useState(false);
     const [loginErrorMsg, setLoginErrorMsg] = useState();
 
