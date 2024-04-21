d91b48da102b48a15dbce5bcc021d1ca8c3252fc
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-20 22:25:20 -0400

    Updated to use new simplified scanner component

--- app/src/pages/CPDash/CPDash_CheckIn.jsx
+++ app/src/pages/CPDash/CPDash_CheckIn.jsx
@@ -9,28 +9,23 @@ import Scanner from '../../components/Scanner';
 const CheckIn = ({ APIServer, allTrackdays }) => {
     const [pendingSubmit, setPendingSubmit] = useState('');
     const [showNotificationModal, setShowNotificationModal] = useState('');
-
     const [failModal, setFailModal] = useState('')
+    let nextTrackday = { date: 'ERROR: NO UPCOMING DATE'}
 
-    const [nextTrackday, setNextTrackday] = useState(''); // Corresponds to next trackday object
-    const [scanData, setScanData] = useState('')
-    const [refreshScanner, setRefreshScanner] = useState(false) // Scanner watches for changes to this which prompts it to start scanning again
-    const [scannerActive, setScannerActive] = useState(true);
-
+    
     // Load in the nextTrackday
-    if (allTrackdays && !nextTrackday) {
+    if (allTrackdays) {
         const lateAllowance = 12 * 60 * 60 * 1000; // Time in ms that a trackday will still be considered the next trackday AFTER it has already passed. Default is 12H
 
         // Remove trackdays in the past
-        allTrackdays = allTrackdays.filter((trackday) => {
+        const upcomingTrackdays = allTrackdays.filter((trackday) => {
             return new Date(trackday.date).getTime() + lateAllowance >= Date.now() // Trackday is in future
         })
-        if (allTrackdays.length == 0) {
-            setNextTrackday({ date: 'ERROR' })
+        if (upcomingTrackdays.length == 0) {
             return console.error('no more TD')// Protect against no trackdays
         }
 
-        allTrackdays.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0))
+        upcomingTrackdays.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0))
 
         // Formatting
         const date = new Date(allTrackdays[0].date)
@@ -38,20 +33,11 @@ const CheckIn = ({ APIServer, allTrackdays }) => {
         const numericDay = date.toLocaleString('default', { day: 'numeric' })
         const formattedDate = month + ' ' + numericDay;
 
-        setNextTrackday({ date: formattedDate, id: allTrackdays[0].id });
+        nextTrackday = { date: formattedDate, id: upcomingTrackdays[0].id };
     }
 
-
-
-    if (scanData && !failModal){
-        const QRData = scanData.replace('https://ride42.ca/dashboard/', '').split('/');
-        setScanData('')
-        setScannerActive(false);
-        handleCheckIn(QRData[0], QRData[1]);
-    }
-
-
-    async function handleCheckIn(userID, bikeID) {
+    async function handleCheckIn(scanData, scanner) {
+        const [userID, bikeID] = scanData.replace("https://ride42.ca/dashboard/", "").split("/");
         setPendingSubmit({ show: true, msg: 'Checking user in' });
         try {
             const response = await fetch(APIServer + 'checkin/' + userID + '/' + nextTrackday.id + '/' + bikeID, {
@@ -64,10 +50,10 @@ const CheckIn = ({ APIServer, allTrackdays }) => {
             setPendingSubmit(''); // Clear loading screen
             if (response.ok) {
                 setShowNotificationModal({ show: true, msg: 'Check-In complete' })
-                setTimeout(() => setScannerActive(true), 2000) // Prompt scanner to start scanning again
+                setTimeout(() => scanner.start(), 2000) // Prompt scanner to start scanning again
             } else if (response.status === 403) {
                 const data = await response.json();
-                setFailModal({ show: true, msg: data.msg })
+                setFailModal({ show: true, msg: data.msg, scanner: scanner })
             } else {
                 throw new Error('API Failure')
             }
@@ -76,22 +62,16 @@ const CheckIn = ({ APIServer, allTrackdays }) => {
         }
     }
 
-
-
-
-
-
-
     return (
         <>
             <ScrollToTop />
             <div className={styles.content}>
                 <h1>{nextTrackday.date} Check In</h1>
-                <Scanner setScanData={setScanData} scannerActive={scannerActive}></Scanner>
+                <Scanner onDecodeEnd={handleCheckIn} />
             </div>
             <Modal open={pendingSubmit.show} type='loading' text={pendingSubmit.msg}></Modal>
             <Modal open={showNotificationModal.show} type='notification' text={showNotificationModal.msg} onClose={() => setShowNotificationModal('')}></Modal>
-            <Modal open={failModal.show} type='confirmation' text={`Error: \n ${failModal.msg}`} onClose={() =>{ setScannerActive(true); setFailModal('')}}
+            <Modal open={failModal.show} type='confirmation' text={`Error: \n ${failModal.msg}`} onClose={() =>{ setFailModal(''); failModal.scanner.start()}}
                 okText="" closeText="Close" ></Modal>
         </>
     );
