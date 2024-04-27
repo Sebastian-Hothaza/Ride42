05b1aff8a1260b48109988eedb2f10773cb154c0
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-27 15:19:44 -0400

    skeleton for download csv

--- app/src/pages/CPDash/CPDash_TrackdaySummary.jsx
+++ app/src/pages/CPDash/CPDash_TrackdaySummary.jsx
@@ -1,43 +1,69 @@
 import { useState } from "react";
 import ScrollToTop from "../../components/ScrollToTop";
 
-import Modal from "../../components/Modal";
-import Loading from '../../components/Loading';
+
 
 import styles from './stylesheets/CPDash_TrackdaySummary.module.css'
-import modalStyles from '../../components/stylesheets/Modal.module.css'
-
-import checkmark from './../../assets/checkmark.png'
-import errormark from './../../assets/error.png'
 
 
-const TrackdaySummary = ({ APIServer, }) => {
+const TrackdaySummary = ({ allUsers, allTrackdaysFULL }) => {
 
-	const [activeModal, setActiveModal] = useState(''); // Tracks what modal should be shown
+	const [selectedTrackdayId, setSelectedTrackdayId] = useState(''); // Tracks what the current working trackdayId is 
+
+	let selectedTrackday;
+
+	if (!allUsers || !allTrackdaysFULL) {
+		return null;
+	} else {
+		allUsers.sort((a, b) => (a.firstName > b.firstName) ? 1 : ((b.firstName > a.firstName) ? -1 : 0))
+		allTrackdaysFULL.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0))
+	}
+
+	// Augment prettydate of allTrackdays to be a nice format
+	allTrackdaysFULL.forEach((trackday) => {
+		const date = new Date(trackday.date)
+		const weekday = date.toLocaleString('default', { weekday: 'short' })
+		const month = date.toLocaleString('default', { month: 'long' })
+		const numericDay = date.toLocaleString('default', { day: 'numeric' })
+		const formattedDate = weekday + ' ' + month + ' ' + numericDay;
+		trackday.prettyDate = formattedDate;
+	})
+
+
+	// Set the selected trackday and sort its members array
+	if (selectedTrackdayId) {
+		selectedTrackday = allTrackdaysFULL.find((td) => td._id === selectedTrackdayId)
+		selectedTrackday.members.sort((a, b) => (a.user.firstName > b.user.firstName) ? 1 : ((b.user.firstName > a.user.firstName) ? -1 : 0))
+	}
+
+
+	// Download CSV file
+	function download() {
+		let result = `Some text here\n`
+
+		const link = window.document.createElement('a');
+		const file = new Blob([result], { type: 'text/csv' });
+		link.href = URL.createObjectURL(file);
+		link.download = `${selectedTrackday.prettyDate}_Summary.csv`;
+		document.body.appendChild(link);
+		link.click();
+	}
 
 	return (
 		<>
-			<ScrollToTop />
 			<div className={styles.content}>
-				<h1>Trackday Summary</h1>
+				<h1>Trackday Summary-
+					<form>
+						<div className={styles.inputPairing}>
+							<select name="trackday" id="trackday" onChange={() => setSelectedTrackdayId(trackday.value)} required>
+								<option style={{ textAlign: 'center' }} key="none" value="">---Select---</option>
+								{allTrackdaysFULL.map((trackday) => <option key={trackday._id} value={trackday._id}>{trackday.prettyDate}</option>)}
+							</select>
+						</div>
+					</form>
+				</h1>
+				{selectedTrackday && <button onClick={() => download()}>Download Summary</button>}
 			</div>
-
-			<Loading open={activeModal.type === 'loading'}>
-				{activeModal.msg}
-			</Loading>
-
-			<Modal open={activeModal.type === 'success'}>
-				<div className={modalStyles.modalNotif}></div>
-				<img id={modalStyles.modalCheckmarkIMG} src={checkmark} alt="checkmark icon" />
-				{activeModal.msg}
-			</Modal>
-
-			<Modal open={activeModal.type === 'failure'}>
-				<div className={modalStyles.modalNotif}></div>
-				<img id={modalStyles.modalCheckmarkIMG} src={errormark} alt="error icon" />
-				{activeModal.msg}
-				<button className='actionButton' onClick={() => setActiveModal('')}>Close</button>
-			</Modal>
 		</>
 	);
 };
