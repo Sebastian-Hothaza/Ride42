3a7b060f815ac2821fad1e9a5ccd432fc0cbf7f9
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-11-26 13:13:49 -0500

    Depracated requestQR feature

--- app/src/pages/CPDash/CPDash_Garage.jsx
+++ app/src/pages/CPDash/CPDash_Garage.jsx
@@ -15,28 +15,6 @@ const Garage = ({ APIServer, userInfo, fetchAPIData, setActiveTab }) => {
 
 	const [activeModal, setActiveModal] = useState(''); // Tracks what modal should be shown
 
-	async function handleRequestQR(bikeID) {
-		setActiveModal({ type: 'loading', msg: 'Requesting your QR Code' });
-		try {
-			const response = await fetch(APIServer + 'qrcode/' + userInfo._id + '/' + bikeID, {
-				method: 'POST',
-				credentials: "include",
-				headers: {
-					'Content-type': 'application/json; charset=UTF-8',
-				}
-			})
-			if (response.ok) {
-				setActiveModal({ type: 'qrConfirm'});
-			} else {
-				const data = await response.json();
-				setActiveModal({ type: 'failure', msg: data.msg.join('\n') })
-			}
-		} catch (err) {
-			setActiveModal({ type: 'failure', msg: 'API Failure' })
-			console.log(err.message)
-		}
-	}
-
 	async function handleRemoveBike(bikeID) {
 		setActiveModal({ type: 'loading', msg: 'Selling your bike' });
 		try {
@@ -102,8 +80,7 @@ const Garage = ({ APIServer, userInfo, fetchAPIData, setActiveTab }) => {
 							<div key={garageItem.bike._id} className={styles.bikeEntry}>
 								<div className='capitalizeEach' >{garageItem.bike.year} {garageItem.bike.make} <span className='capitalizeAll'>{garageItem.bike.model}</span></div>
 								<div className={styles.bikeControls}>
-									<button onClick={(e) => handleRequestQR(garageItem.bike._id)}>Request replacement sticker</button>
-									<button className={styles.confirmBtn} onClick={(e) => handleRemoveBike(garageItem.bike._id)}>Remove</button>
+									<button className={styles.confirmBtn} onClick={(e) => handleRemoveBike(garageItem.bike._id)}>Remove Bike</button>
 								</div>
 
 							</div>
@@ -154,18 +131,13 @@ const Garage = ({ APIServer, userInfo, fetchAPIData, setActiveTab }) => {
 
 			<Modal open={activeModal.type === 'addBikeConfirm'}>
 				<>
-					Your bike has been added to your garage. We have created a QR code for you which will be available for pickup at the next trackday.
+					Your bike has been added to your garage. Please collect your QR sticker at the next trackday.
 					<button className={`actionButton ${styles.confirmBtn}`} onClick={()=>setActiveTab('trackdays')}>Go to My Trackdays</button>
 					<button className='actionButton' onClick={()=>setActiveModal('')}>Stay in garage</button>
 				</>
 			</Modal>
 
-			<Modal open={activeModal.type === 'qrConfirm'}>
-				<>
-					We have created a QR code for you which will be available for pickup at the next trackday.
-					<button className='actionButton' onClick={() => setActiveModal('')}>Ok</button>
-				</>
-			</Modal>
+
 		</>
 
 
