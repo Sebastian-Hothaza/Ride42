666c15d61040a6303adffd7a30c8d62f9e88cc10
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-11-28 16:13:33 -0500

    Updated logging logic

--- api/mongoConfig.js
+++ api/mongoConfig.js
@@ -3,7 +3,8 @@ const logger = require('./logger');
 
 main().catch((err) => logger.error({message: err}));
 async function main() { 
-  if (process.env.NODE_ENV !== 'test') await mongoose.connect(process.env.MONGODB_URI);
-  if (process.env.NODE_ENV === 'development') logger.info({message: 'DB Connection established'});
-  
+  if (process.env.NODE_ENV !== 'test') {
+    await mongoose.connect(process.env.MONGODB_URI);
+    logger.debug({message: 'DB Connection established'});
+  }
 }
