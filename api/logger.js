9108f43d8a8ea0932199480fac3191ce0a09bfc1
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-11-28 15:47:33 -0500

    Added transport for mongoDB logs

--- api/logger.js
+++ api/logger.js
@@ -1,4 +1,5 @@
 const winston = require('winston');
+require('winston-mongodb');
 
 winston.addColors({
     error: 'bold white redBG',
@@ -9,7 +10,6 @@ winston.addColors({
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
     levels: {
         error: 0,
         warn: 1,
@@ -20,9 +20,21 @@ const logger = winston.createLogger({
 
 // Log to console in dev, stream to mongoDB in prod
 if (process.env.NODE_ENV === 'development') {
-    logger.add(new winston.transports.Console( { format: winston.format.colorize({ all: true }) } ));
+    logger.add(new winston.transports.Console({ format: winston.format.colorize({ all: true }) }));
 } else if (process.env.NODE_ENV === 'production') {
     // Add mongoDB transport
+    logger.add(new winston.transports.MongoDB({
+        db: process.env.MONGODB_URI,
+        collection: 'serverLogs',
+        capped: true,
+        cappedMax: 10000,
+        tryReconnect: true
+    }));
 }
 
 module.exports = logger;
+
+/*
+Note; db should be MongoDB connection uri, pre-connected MongoClient object or promise which resolves to a pre-connected MongoClient object.
+Optimization note: we are double connecting to DB here?
+*/
