684f890b3580905f5f94d75b558b05e3ec80061f
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-11-29 17:17:00 -0500

    added card for bundles

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Card from "../components/Card"
 
 import about from '../assets/about.png'
 import offer from '../assets/offer.jpg'
+import bundle from '../assets/bundle.jpg'
 
 import raceway from '../assets/raceway.png'
 import partners from '../assets/partners.png'
@@ -12,6 +13,20 @@ import styles from './stylesheets/Home.module.css'
 
 
 const Home = () => {
+
+	const HTML_Bundle = <div>
+		<h2>Rider friendly bundles are now available for purchase! 🚀</h2>
+		<h3> What makes these 3-day bundles friendly? 😁</h3>
+		<br></br>
+		<ul>
+			<li>Save $120 vs registering at gate for each date individually</li>
+			<li>No 7-day notice needed. Simply show up and RIDE! </li>
+			<li>3-day bundle is <b>$450</b> all in</li>
+		</ul>
+		<br></br>
+		<button className={styles.bookBtn} onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSceY0VQfJmDCMTTNq0RWReTi3kn3Jih9MQo71RLqDcTBKVOTQ/viewform?usp=sf_link', '_blank')}>Get your bundle now!</button>
+	</div>
+
 	const HTML_AboutUs = <p>
 		Founded in 2024, Ride42 primarily focuses on running motorcycle trackdays at Grand Bend Motorplex. Many riders begin their motorcycle journey on the street
 		and soon realize the danger of exploring the limits of their machine on public roads with other road users, unknown road surface conditions and no run off
@@ -41,7 +56,7 @@ const Home = () => {
 			<li>Experienced pro level regional and national racers available for coaching and guidance</li>
 			<li>BBQ lunch with refreshments</li>
 			<li>Paved, well maintained track environment without the hazards, limitations or distractions you’d typically find on the street</li>
-			
+
 		</ul>
 	</div>
 
@@ -54,8 +69,9 @@ const Home = () => {
 				</div>
 			</div>
 			<div className="content" id={styles.firstCard}>
-				<Card heading='About Us' body={HTML_AboutUs} img={about} inverted={false} />
-				<Card heading='What We Offer' body={HTML_Offer} img={offer} inverted={true} />
+				<Card heading='2025 Season Bundle' body={HTML_Bundle} img={bundle} inverted={false} />
+				<Card heading='About Us' body={HTML_AboutUs} img={about} inverted={true} />
+				<Card heading='What We Offer' body={HTML_Offer} img={offer} inverted={false} />
 				<div className={styles.imageContainer}>
 					<img src={partners} alt="Picture of our partners" />
 					<img src={raceway} alt="Picture of Grand Bend Track" />
