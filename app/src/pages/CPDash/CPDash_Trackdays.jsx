0cd7421261822f174a6a54a392504b5bfadd51ca
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-11-29 15:17:49 -0500

    removed centering on register button

--- app/src/pages/CPDash/CPDash_Trackdays.jsx
+++ app/src/pages/CPDash/CPDash_Trackdays.jsx
@@ -265,7 +265,7 @@ const Trackdays = ({ APIServer, userInfo, allTrackdays, userTrackdays, fetchAPID
 					<div className={styles.layoutVote}>
 						<legend><h3>Do you understand our rules and policies?</h3></legend>
 						<div className={styles.checkboxes}>
-							<div className={styles.checkboxPairing} style={{ justifySelf: 'center' }}>
+							<div className={styles.checkboxPairing}>
 								<input type="checkbox" id="ruleAgree" name="ruleAgree" required></input>
 								<label htmlFor="ruleAgree">Yes, I have read and agree to them</label>
 							</div>
