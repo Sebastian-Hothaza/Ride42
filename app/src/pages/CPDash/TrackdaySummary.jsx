1cc7f4d78c75984754732cc5de877b5f99657d9f
Sebastian Hothaza <sebastianhothaza@gmail.com>
2025-01-08 16:32:01 -0500

    Added core information for displaying financial info

--- app/src/pages/CPDash/TrackdaySummary.jsx
+++ app/src/pages/CPDash/TrackdaySummary.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import ScrollToTop from "../../components/ScrollToTop";
 
 
@@ -16,7 +16,7 @@ const TrackdaySummary = ({ allUsers, allTrackdaysFULL }) => {
 		return null;
 	} else {
 		allUsers.sort((a, b) => (a.firstName > b.firstName) ? 1 : ((b.firstName > a.firstName) ? -1 : 0))
-		allTrackdaysFULL = allTrackdaysFULL.filter(trackday => trackday.status != "archived"); // exclude archived trackdays
+		//allTrackdaysFULL = allTrackdaysFULL.filter(trackday => trackday.status != "archived"); // exclude archived trackdays
 		allTrackdaysFULL.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0))
 	}
 
@@ -31,11 +31,51 @@ const TrackdaySummary = ({ allUsers, allTrackdaysFULL }) => {
 	})
 
 
-	// Set the selected trackday and sort its members array
+	// Set the selected trackday and sort its members array. Augment object to include values for display
 	if (selectedTrackdayId) {
 		selectedTrackday = allTrackdaysFULL.find((td) => td._id === selectedTrackdayId)
 		selectedTrackday.members.sort((a, b) => (a.user.firstName > b.user.firstName) ? 1 : ((b.user.firstName > a.user.firstName) ? -1 : 0))
+
+		// Adding ticket prices
+		selectedTrackday.preRegPrice = selectedTrackday.ticketPrice.preReg;
+		selectedTrackday.gatePrice = selectedTrackday.ticketPrice.gate;
+		selectedTrackday.bundlePrice = selectedTrackday.ticketPrice.bundle;
+
+		// Adding ticket quantities
+		selectedTrackday.preRegQty = selectedTrackday.members.filter((member) => member.paymentMethod == 'etransfer' || member.paymentMethod == 'creditCard').length;
+		selectedTrackday.gateQty = selectedTrackday.members.filter((member) => member.paymentMethod == 'gate').length + selectedTrackday.walkons.length;
+		selectedTrackday.bundleQty = selectedTrackday.members.filter((member) => member.paymentMethod == 'credit' && member.user.memberType == 'regular').length;
+
+		// Totals
+		selectedTrackday.totalRevenue = (selectedTrackday.preRegPrice * selectedTrackday.preRegQty) + (selectedTrackday.gatePrice * selectedTrackday.gateQty) + (selectedTrackday.bundlePrice * selectedTrackday.bundleQty)
+		selectedTrackday.totalExpense = 0;
+
+		// Add additional revenue
+		selectedTrackday.costs.filter((costObject) => costObject.amount < 0).map((costObject) => {
+			if (costObject.type == 'fixed') {
+				selectedTrackday.totalRevenue += costObject.amount * -1;
+			} else {
+				selectedTrackday.totalRevenue += costObject.amount * -1 * (selectedTrackday.members.length + selectedTrackday.walkons.length);
+			}
+		})
+
+		// Add additional expenses
+		selectedTrackday.costs.filter((costObject) => costObject.amount > 0).map((costObject) => {
+			if (costObject.desc == 'BBQ'){
+				selectedTrackday.totalExpense += costObject.amount * selectedTrackday.guests;
+			} else if (costObject.type == 'fixed') {
+				selectedTrackday.totalExpense += costObject.amount;
+			} else {
+				selectedTrackday.totalExpense += costObject.amount * (selectedTrackday.members.length + selectedTrackday.walkons.length);
+			}
+		})
+
+
+
+
 	}
+
+
 
 
 	// Download CSV file
@@ -63,7 +103,72 @@ const TrackdaySummary = ({ allUsers, allTrackdaysFULL }) => {
 						</div>
 					</form>
 				</h1>
-				{selectedTrackday && <button onClick={() => download()}>Download Summary</button>}
+				{selectedTrackday &&
+
+					<div className={styles.reportGrid}>
+						<h2>Revenue</h2>
+
+						<div>Pre-Reg Ticket Sales</div>
+						<div>${selectedTrackday.preRegPrice} x {selectedTrackday.preRegQty}</div>
+						<div>${selectedTrackday.preRegPrice * selectedTrackday.preRegQty}</div>
+
+						<div>Gate Ticket Sales</div>
+						<div>${selectedTrackday.gatePrice} x {selectedTrackday.gateQty}</div>
+						<div>${selectedTrackday.gatePrice * selectedTrackday.gateQty}</div>
+
+						<div>Bundle Ticket Sales</div>
+						<div>${selectedTrackday.bundlePrice} x {selectedTrackday.bundleQty}</div>
+						<div>${selectedTrackday.bundlePrice * selectedTrackday.bundleQty}</div>
+
+						{selectedTrackday.costs.filter((costObject) => costObject.amount < 0).map((costObject) => {
+							return (
+								<React.Fragment key={costObject._id}>
+									<div >{costObject.desc}</div>
+									{costObject.type == 'variable' ? <div >${costObject.amount * -1} x {selectedTrackday.members.length + selectedTrackday.walkons.length}</div> : <div ></div>}
+									{costObject.type == 'variable' ? <div >${costObject.amount * -1 * (selectedTrackday.members.length + selectedTrackday.walkons.length)}</div> : <div>${costObject.amount * -1}</div>}
+								</React.Fragment>
+							)
+						})}
+
+						<h3>Total Revenue</h3>
+						<h3> </h3>
+						<h3>${selectedTrackday.totalRevenue}</h3>
+
+						<h2>Expenses</h2>
+						<div>Track Rental</div>
+						<div></div>
+						<div>${selectedTrackday.costs.find((costObject) => costObject.desc == 'trackRental').amount}</div>
+
+						{selectedTrackday.costs.filter((costObject) => costObject.amount > 0 && costObject.desc != 'trackRental').map((costObject) => {
+							return (
+								<React.Fragment key={costObject._id}>
+									<div >{costObject.desc}</div>
+									{costObject.desc == 'BBQ' ? <div>${costObject.amount} x {selectedTrackday.guests}</div> :
+										costObject.type == 'variable' ? <div >${costObject.amount} x {selectedTrackday.members.length + selectedTrackday.walkons.length}</div> : <div ></div>
+									}
+
+									{costObject.desc == 'BBQ' ? <div>${costObject.amount * selectedTrackday.guests}</div> :
+										costObject.type == 'variable' ? <div >${costObject.amount * (selectedTrackday.members.length + selectedTrackday.walkons.length)}</div> : <div>${costObject.amount}</div>
+									}
+
+
+								</React.Fragment>
+							)
+						})}
+
+						<h3>Total Expenses</h3>
+						<h3> </h3>
+						<h3>${selectedTrackday.totalExpense}</h3>
+
+						<h3>Profit</h3>
+						<h3> </h3>
+						<h3>${selectedTrackday.totalRevenue - selectedTrackday.totalExpense}</h3>
+					</div>
+
+
+
+
+				}
 			</div>
 		</>
 	);
