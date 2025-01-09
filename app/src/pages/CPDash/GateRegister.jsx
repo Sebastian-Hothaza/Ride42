a283b6b843dbe913f1c2818cf4e4f231eb402398
Sebastian Hothaza <sebastianhothaza@gmail.com>
2025-01-09 12:02:31 -0500

    Added ability to change date we are submitting gate registration for

--- app/src/pages/CPDash/GateRegister.jsx
+++ app/src/pages/CPDash/GateRegister.jsx
@@ -14,33 +14,40 @@ import errormark from './../../assets/error.png'
 const GateRegister = ({ APIServer, fetchAPIData, allUsers, allTrackdays }) => {
 
     const [activeModal, setActiveModal] = useState(''); // Tracks what modal should be shown
-
     const [eligibleUsers, setEligibleUsers] = useState('');
+    const [selectedTrackdayId, setSelectedTrackdayId] = useState(''); // Tracks what the current working trackdayId is 
 
-    let nextTrackday = { date: 'ERROR: NO UPCOMING DATE' }
+    // Augment prettydate of allTrackdays to be a nice format
+    allTrackdays.forEach((trackday) => {
+        const date = new Date(trackday.date)
+        const weekday = date.toLocaleString('default', { weekday: 'short' })
+        const month = date.toLocaleString('default', { month: 'long' })
+        const numericDay = date.toLocaleString('default', { day: 'numeric' })
+        const formattedDate = weekday + ' ' + month + ' ' + numericDay;
+        trackday.prettyDate = formattedDate;
+    })
 
-    // Load in the nextTrackday
-    if (allTrackdays) {
+    // Removed archived trackdays
+    allTrackdays = allTrackdays.filter((td) => td.status != 'archived')
+
+    // Set the selected trackday 
+    let selectedTrackday;
+    if (selectedTrackdayId) selectedTrackday = allTrackdays.find((td) => td.id === selectedTrackdayId)
+
+    // Load in the selectedTrackday to default value
+    if (allTrackdays && !selectedTrackdayId) {
         const lateAllowance = 12 * 60 * 60 * 1000; // Time in ms that a trackday will still be considered the next trackday AFTER it has already passed. Default is 12H
 
         // Remove trackdays in the past
         const upcomingTrackdays = allTrackdays.filter((trackday) => {
             return new Date(trackday.date).getTime() + lateAllowance >= Date.now() // Trackday is in future
         })
-        if (upcomingTrackdays.length == 0) {
-            return console.error('no more TD')// Protect against no trackdays
-        }
+        if (upcomingTrackdays.length == 0) return console.error('no more TD')// Protect against no trackdays
 
         upcomingTrackdays.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0))
-
-        // Formatting
-        const date = new Date(upcomingTrackdays[0].date)
-        const month = date.toLocaleString('default', { month: 'long' })
-        const numericDay = date.toLocaleString('default', { day: 'numeric' })
-        const formattedDate = month + ' ' + numericDay;
-
-        nextTrackday = { date: formattedDate, id: upcomingTrackdays[0].id };
+        setSelectedTrackdayId(upcomingTrackdays[0].id);
     }
+
 
     // Load in eligible users (sorted by first name). 
     if (allUsers && !eligibleUsers) {
@@ -71,7 +78,7 @@ const GateRegister = ({ APIServer, fetchAPIData, allUsers, allTrackdays }) => {
     async function handleRegistrationSubmit(userID) {
         setActiveModal({ type: 'loading', msg: 'Submitting gate registration' });
         try {
-            const response = await fetch(APIServer + 'register/' + userID + '/' + nextTrackday.id, {
+            const response = await fetch(APIServer + 'register/' + userID + '/' + selectedTrackday.id, {
                 method: 'POST',
                 credentials: "include",
                 headers: {
@@ -102,7 +109,7 @@ const GateRegister = ({ APIServer, fetchAPIData, allUsers, allTrackdays }) => {
         setActiveModal({ type: 'loading', msg: 'Submitting walk-on registration' });
         const formData = new FormData(e.target);
         try {
-            const response = await fetch(APIServer + 'walkons/' + nextTrackday.id, {
+            const response = await fetch(APIServer + 'walkons/' + selectedTrackday.id, {
                 method: 'POST',
                 credentials: "include",
                 headers: {
@@ -122,6 +129,7 @@ const GateRegister = ({ APIServer, fetchAPIData, allUsers, allTrackdays }) => {
             setActiveModal({ type: 'failure', msg: 'API Failure' })
             console.log(err.message)
         }
+        e.target.reset();
     }
 
     // Called by filter input everytime there is a change, filters by last 4 digits of phone number
@@ -130,67 +138,83 @@ const GateRegister = ({ APIServer, fetchAPIData, allUsers, allTrackdays }) => {
         setEligibleUsers(allUsers.filter((user) => input.value === user.contact.phone.slice(6, 6 + input.value.length)))
     }
 
-
-
     return (
         <>
             <ScrollToTop />
-            <h1>Gate Registration for {nextTrackday.date}</h1>
-            
-            <div className={styles.content}>
-                <h3>Existing Members</h3>
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    // If user has no waiver completed, show modal. Else proceed directly with gate reg
-                    if (allUsers.find((user) => user._id === e.target.user.value && user.waiver === false)) {
-                        setActiveModal({ type: 'waiverWarning', userID: e.target.user.value });
-                    } else {
-                        handleRegistrationSubmit(e.target.user.value)
-                    }
-                    e.target.reset();
-                    setEligibleUsers(allUsers);
-                }}>
+            {selectedTrackday &&
+                <>
+                    <div className={styles.content}>
+                        <h1>Gate Register -
+                            <form>
+                                <div className={styles.inputPairing}>
+                                    <select name="trackday" id="trackday" value={selectedTrackday.id} onChange={() => setSelectedTrackdayId(trackday.value)} required>
+                                        {allTrackdays.map((trackday) => <option key={trackday.id} value={trackday.id}>{trackday.prettyDate}</option>)}
+                                    </select>
+                                </div>
+                            </form>
+                        </h1>
 
-                    <label htmlFor="year">Search members by last 4 digits of phone number:</label>
-                    <input type="number" id="phoneEnd" name="phoneEnd" onInput={filterEligibleUsers}></input>
+                        <div>
+                            <h3>Existing Members</h3>
+                            <form onSubmit={(e) => {
+                                e.preventDefault();
+                                // If user has no waiver completed, show modal. Else proceed directly with gate reg
+                                if (allUsers.find((user) => user._id === e.target.user.value && user.waiver === false)) {
+                                    setActiveModal({ type: 'waiverWarning', userID: e.target.user.value });
+                                } else {
+                                    handleRegistrationSubmit(e.target.user.value)
+                                }
+                                e.target.reset();
+                                setEligibleUsers(allUsers);
+                            }}>
+
+                                <label htmlFor="year">Search members by last 4 digits of phone number:</label>
+                                <input type="number" id="phoneEnd" name="phoneEnd" onInput={filterEligibleUsers}></input>
 
 
-                    <select className='capitalizeEach' name="user" id="user" required>
-                        {eligibleUsers && eligibleUsers.map((user) => <option className='capitalizeEach' key={user._id} value={user._id}>{user.firstName}, {user.lastName}</option>)}
-                        {eligibleUsers.length == 0 && <option key='empty' value=''>No entries found</option>}
-                    </select>
+                                <select className='capitalizeEach' name="user" id="user" required>
+                                    {eligibleUsers && eligibleUsers.map((user) => <option className='capitalizeEach' key={user._id} value={user._id}>{user.firstName}, {user.lastName}</option>)}
+                                    {eligibleUsers.length == 0 && <option key='empty' value=''>No entries found</option>}
+                                </select>
 
-                    <button className={styles.confirmBtn} type="submit">Gate Register</button>
-                </form>
-            </div>
+                                <button className={styles.confirmBtn} type="submit">Gate Register</button>
+                            </form>
+                        </div>
+
+                        <div>
+                            <h3>Walk-Ons</h3>
+                            <form onSubmit={(e) => handleWalkOnSubmit(e)}>
+                                <div className={styles.inputPairing}>
+                                    <label htmlFor="firstName">First Name:</label>
+                                    <input type="text" autoComplete='off' id="firstName" name="firstName" required minLength={2} maxLength={50}></input>
+                                </div>
+
+                                <div className={styles.inputPairing}>
+                                    <label htmlFor="lastName">Last Name:</label>
+                                    <input type="text" autoComplete='off' id="lastName" name="lastName" required minLength={2} maxLength={50}></input>
+                                </div>
+
+                                <div className={styles.inputPairing}>
+                                    <label htmlFor="group">Group:</label>
+                                    <select name="group" id="group" form={styles.registerForm} required>
+                                        <option key="groupNone" value="">---Choose Group---</option>
+                                        <option key="green" value="green">Green</option>
+                                        <option key="yellow" value="yellow">Yellow</option>
+                                        <option key="red" value="red">Red</option>
+                                    </select>
+                                </div>
+                                <button className={styles.confirmBtn} type="submit">Register Walk-On</button>
+                            </form>
+                        </div>
 
 
-
-            <div className={styles.content}>
-                <h3>Walk-Ons</h3>
-                <form onSubmit={(e) => handleWalkOnSubmit(e)}>
-                    <div className={styles.inputPairing}>
-                        <label htmlFor="firstName">First Name:</label>
-                        <input type="text" id="firstName" name="firstName" required minLength={2} maxLength={50}></input>
                     </div>
 
-                    <div className={styles.inputPairing}>
-                        <label htmlFor="lastName">Last Name:</label>
-                        <input type="text" id="lastName" name="lastName" required minLength={2} maxLength={50}></input>
-                    </div>
 
-                    <div className={styles.inputPairing}>
-                        <label htmlFor="group">Group:</label>
-                        <select name="group" id="group" form={styles.registerForm} required>
-                            <option key="groupNone" value="">---Choose Group---</option>
-                            <option key="green" value="green">Green</option>
-                            <option key="yellow" value="yellow">Yellow</option>
-                            <option key="red" value="red">Red</option>
-                        </select>
-                    </div>
-                    <button className={styles.confirmBtn} type="submit">Register Walk-On</button>
-                </form>
-            </div>
+
+
+                </>
+            }
 
 
             <Loading open={activeModal.type === 'loading'}>
