68e8d8c9238b2eb59f6cdde1c3fe2091b1144866
Sebastian Hothaza <sebastianhothaza@gmail.com>
2025-01-09 13:51:41 -0500

    Updated to allow changing dates

--- app/src/pages/CPDash/Verify.jsx
+++ app/src/pages/CPDash/Verify.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ScrollToTop from "../../components/ScrollToTop";
 
 import Modal from "../../components/Modal";
@@ -15,51 +15,57 @@ import errormark from './../../assets/error.png'
 
 const Verify = ({ APIServer, allTrackdays, allUsers }) => {
     const [activeModal, setActiveModal] = useState(''); // Tracks what modal should be shown
+    const [selectedTrackdayId, setSelectedTrackdayId] = useState(''); // Tracks what the current working trackdayId is 
+    const selectedTrackdayRef = useRef(null); // Ref to keep track of the latest selectedTrackday
 
-    let nextTrackday = { date: 'ERROR: NO UPCOMING DATE' }
 
-    // Load in the nextTrackday
-    if (allTrackdays) {
-        const lateAllowance = 12 * 60 * 60 * 1000; // Time in ms that a trackday will still be considered the next trackday AFTER it has already passed. Default is 12H
-
-        // Remove trackdays in the past
-        const upcomingTrackdays = allTrackdays.filter((trackday) => {
-            return new Date(trackday.date).getTime() + lateAllowance >= Date.now() // Trackday is in future
-        })
-        if (upcomingTrackdays.length == 0) {
-            return console.error('no more TD')// Protect against no trackdays
-        }
-
-        upcomingTrackdays.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0))
-
-        // Formatting
-        const date = new Date(upcomingTrackdays[0].date)
+    // Augment prettydate of allTrackdays to be a nice format
+    allTrackdays.forEach((trackday) => {
+        const date = new Date(trackday.date)
+        const weekday = date.toLocaleString('default', { weekday: 'short' })
         const month = date.toLocaleString('default', { month: 'long' })
         const numericDay = date.toLocaleString('default', { day: 'numeric' })
-        const formattedDate = month + ' ' + numericDay;
+        const formattedDate = weekday + ' ' + month + ' ' + numericDay;
+        trackday.prettyDate = formattedDate;
+    })
 
-        nextTrackday = { date: formattedDate, id: upcomingTrackdays[0].id };
+    // Removed archived trackdays
+    allTrackdays = allTrackdays.filter((td) => td.status != 'archived')
+
+    // Set the ref
+    if (selectedTrackdayId) { // Once we have defined trackday to use
+        selectedTrackdayRef.current = allTrackdays.find((td) => td.id === selectedTrackdayId); // Update the ref with the latest selectedTrackday
+    } else if (allTrackdays) {  // Load in the selectedTrackday to default value 
+        const lateAllowance = 12 * 60 * 60 * 1000; // Time in ms that a trackday will still be considered the next trackday AFTER it has already passed. Default is 12H
+        // Remove trackdays in the past
+        const upcomingTrackdays = allTrackdays.filter((trackday) => {
+            return new Date(trackday.date).getTime() + lateAllowance >= Date.now(); // Trackday is in the future
+        });
+        if (upcomingTrackdays.length === 0) return console.error('no more TD'); // Protect against no trackdays
+
+        upcomingTrackdays.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0));
+        setSelectedTrackdayId(upcomingTrackdays[0].id); // This prompts render and sets ref
     }
 
     async function handleVerify(scanData, scanner) {
         let user, bike, APIURL;
         if (scanData.includes('https://ride42.ca/dashboard/')) {
             const [userID, bikeID] = scanData.replace("https://ride42.ca/dashboard/", "").split("/");
-            user = allUsers.find((user)=> user._id === userID)
-            bike = user.garage.find((garageItem)=>garageItem.bike._id===bikeID).bike
-            APIURL = APIServer + 'verify/' + user._id + '/' + nextTrackday.id + '/' + bike._id;
+            user = allUsers.find((user) => user._id === userID)
+            bike = user.garage.find((garageItem) => garageItem.bike._id === bikeID).bike
+            APIURL = APIServer + 'verify/' + user._id + '/' + selectedTrackdayRef.current.id + '/' + bike._id;
         } else {
             const QRID = scanData.replace("https://Ride42.ca/QR/", "")
             user = allUsers.find(user => {
                 const garageItem = user.garage.find(item => item.QRID === QRID);
                 if (garageItem) bike = garageItem.bike
-                return garageItem ? user:undefined;
+                return garageItem ? user : undefined;
             });
-            APIURL = APIServer + 'verify/' + QRID + '/' + nextTrackday.id;
+            APIURL = APIServer + 'verify/' + QRID + '/' + selectedTrackdayRef.current.id;
         }
-  
 
-  
+
+
         setActiveModal({ type: 'loading', msg: 'Verifying user' }); // Show loading modal
         try {
             const response = await fetch(APIURL, {
@@ -96,11 +102,20 @@ const Verify = ({ APIServer, allTrackdays, allUsers }) => {
     return (
         <>
             <ScrollToTop />
-
-            <div className={styles.content}>
-                <h1>{nextTrackday.date} Verify</h1>
-                <Scanner onDecodeEnd={handleVerify} />
-            </div>
+            {selectedTrackdayRef.current &&
+                <div className={styles.content}>
+                    <h1>Verify -
+                        <form>
+                            <div className={styles.inputPairing}>
+                                <select name="trackday" id="trackday" value={selectedTrackdayRef.current.id} onChange={() => setSelectedTrackdayId(trackday.value)} required>
+                                    {allTrackdays.map((trackday) => <option key={trackday.id} value={trackday.id}>{trackday.prettyDate}</option>)}
+                                </select>
+                            </div>
+                        </form>
+                    </h1>
+                    <Scanner onDecodeEnd={handleVerify} />
+                </div>
+            }
 
             <Loading open={activeModal.type === 'loading'}>
                 {activeModal.msg}
