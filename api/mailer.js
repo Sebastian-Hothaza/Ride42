0fc56939228a0a3572c3b8375c858ba4c0d9d154
Sebastian Hothaza <sebastianhothaza@gmail.com>
2025-01-19 23:16:40 -0500

    Add BCC of ride42 so we can track emails sent out

--- api/mailer.js
+++ api/mailer.js
@@ -19,7 +19,7 @@ async function main(recipient,subject,htmlBody,args){
     // Check if args were received, and if so, inject them into the HTML body
     if (args && Object.keys(args).length){
         Object.keys(args).forEach((arg)=>{
-            htmlBody = htmlBody.replace("{"+arg+"}", args[arg])
+            htmlBody = htmlBody.replaceAll("{"+arg+"}", args[arg])
         })
     }
     if (process.env.NODE_ENV === 'production'){
@@ -27,6 +27,7 @@ async function main(recipient,subject,htmlBody,args){
             await transporter.sendMail({
                 from: '"Ride42" <info@ride42.ca>', 
                 to: recipient, 
+                bcc: 'nodemailer@ride42.ca',
                 attachments: [{
                     filename: 'ride42.png',
                     path: 'ride42.png',
