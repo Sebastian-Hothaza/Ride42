c5f3f202e8a599b36637b359a9d943e8d8e28be9
Sebastian Hothaza <sebastianhothaza@gmail.com>
2025-01-23 13:37:51 -0500

    added filter so we only print debug logs to console in prod environment

--- api/logger.js
+++ api/logger.js
@@ -20,6 +20,11 @@ const logger = winston.createLogger({
     format: winston.format.printf(({ level, message }) => { return `${level}: ${message}` }),
 });
 
+// USed to filter out any logs that are not debug logs
+const debugFilter = winston.format((info, opts) => {
+    return info.level === 'debug' ? info : false;
+});
+
 // Log to console in dev, stream to mongoDB in prod
 if (process.env.NODE_ENV === 'production') {
     // Add mongoDB transport
@@ -31,15 +36,21 @@ if (process.env.NODE_ENV === 'production') {
         tryReconnect: true,
         level: 'info' // Minimum log level to store; we dont want to store debug info
     }));
+
     // Add console transport for debug level
-    logger.add(new winston.transports.Console({ level: 'debug', format: winston.format.colorize({ all: true }) }));
+    logger.add(new winston.transports.Console({
+        level: 'debug', format: winston.format.combine(
+            debugFilter(),
+            winston.format.colorize({ all: true })
+        )
+    }));
 } else {
     logger.add(new winston.transports.Console({ format: winston.format.colorize({ all: true }) }));
     if (process.env.NODE_ENV === 'test') logger.level = 'warn'; // Supress console of info messages
 }
 
 module.exports = logger;
- 
+
 /*
 Note; db should be MongoDB connection uri, pre-connected MongoClient object or promise which resolves to a pre-connected MongoClient object.
 Optimization note: we are double connecting to DB here?
