9aa93e1f9a66c4fb1e1dfa15d9c1fb638ebcbf73
Sebastian Hothaza <sebastianhothaza@gmail.com>
2025-01-23 00:54:43 -0500

    added mongoConfig shutdown template, not yet in use

--- api/mongoConfig.js
+++ api/mongoConfig.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 const logger = require('./logger');
 
-main().catch((err) => logger.error({message: err}));
-async function main() { 
+main().catch((err) => logger.error({ message: err }));
+async function main() {
   if (process.env.NODE_ENV !== 'test') {
     await mongoose.connect(process.env.MONGODB_URI);
-    logger.debug({message: 'DB Connection established'});
+    logger.debug({ message: 'DB Connection established' });
   }
 }
+
+/*
+// Handle graceful shutdown
+process.on('SIGINT', async () => {
+  console.log('killing mongoose')
+  await mongoose.disconnect()
+})
+  */
