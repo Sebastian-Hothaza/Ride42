4e6c2097f9c24e0fe60c993b8163f469b1f95fd0
Sebastian Hothaza <sebastianhothaza@gmail.com>
2025-02-26 12:23:18 -0500

    documentation update

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -4,11 +4,6 @@ import Router from "./Router";
 
 import './index.css'
 
-/*
-TODO:
-dates page should indicate if trackday is cancelled (layout should say cancelled in that case)
-*/
-
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Router />
