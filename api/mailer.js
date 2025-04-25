75fd0935025f6260e12ce726c11c23cd0fe11209
Sebastian Hothaza <sebastianhothaza@gmail.com>
2025-04-25 11:44:18 -0400

    added feature to allow sending attachments

--- api/mailer.js
+++ api/mailer.js
@@ -5,42 +5,47 @@ const logger = require('./logger');
 
 // Configure email sender info
 const transporter = nodemailer.createTransport({
-  host: process.env.ADMIN_EMAIL_HOST,
-  port: 465,
-  secure: true,
-  auth: {
-	user: process.env.ADMIN_EMAIL,
-	pass: process.env.ADMIN_EMAIL_PASSWORD,
-  },
+    host: process.env.ADMIN_EMAIL_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.ADMIN_EMAIL,
+        pass: process.env.ADMIN_EMAIL_PASSWORD,
+    },
 });
 
 // Attempt to send the email
-async function main(recipient,subject,htmlBody,args){
+async function main(recipient, subject, htmlBody, args, attachments = []) {
     // Check if args were received, and if so, inject them into the HTML body
-    if (args && Object.keys(args).length){
-        Object.keys(args).forEach((arg)=>{
-            htmlBody = htmlBody.replaceAll("{"+arg+"}", args[arg])
+    if (args && Object.keys(args).length) {
+        Object.keys(args).forEach((arg) => {
+            htmlBody = htmlBody.replaceAll("{" + arg + "}", args[arg])
         })
     }
-    if (process.env.NODE_ENV === 'production'){
-        try{
-            await transporter.sendMail({
-                from: '"Ride42" <info@ride42.ca>', 
-                to: recipient, 
-                bcc: 'nodemailer@ride42.ca',
-                attachments: [{
+    if (true || process.env.NODE_ENV === 'production') {
+        try {
+            const emailAttachments = recipient !== 'waiver@ride42.ca' ? [
+                {
                     filename: 'ride42.png',
                     path: 'ride42.png',
-                    cid: 'sigImg' 
-                }],
-                subject: subject, 
-                html: htmlBody, 
+                    cid: 'sigImg'
+                },
+                ...attachments,
+            ] : attachments;
+
+            await transporter.sendMail({
+                from: '"Ride42" <info@ride42.ca>',
+                to: recipient,
+                bcc: 'nodemailer@ride42.ca',
+                attachments: emailAttachments,
+                subject: subject,
+                html: htmlBody,
             });
-        }catch(err){
-            logger.error({message: err})
+        } catch (err) {
+            logger.error({ message: err })
         }
-    }else if (process.env.NODE_ENV === 'development'){
-        logger.debug({message: `Email sent: \n${htmlBody}`});
+    } else if (process.env.NODE_ENV === 'development') {
+        logger.debug({ message: `Email sent: \n${htmlBody}` });
     }
 
 }
