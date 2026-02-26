811a3acc1277dae1bac4c4f110e905c49cdbef07
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-02-26 17:03:44 -0500

    added validateOrderID

--- api/controllers/controllerUtils.js
+++ api/controllers/controllerUtils.js
@@ -2,7 +2,8 @@ const User = require('../models/User');
 const Trackday = require('../models/Trackday');
 const Bike = require('../models/Bike');
 const QR = require('../models/QR');
-const Product = require('../models/Products');
+const { Product } = require('../models/Products');
+const Order = require('../models/Orders');
 const { body, validationResult } = require("express-validator");
 const ObjectId = require('mongoose').Types.ObjectId;
 const jwt = require('jsonwebtoken')
@@ -80,11 +81,20 @@ async function validateProductID(req, res, next) {
     next();
 }
 
+// Called by middleware functions
+// Verify that the req.params.orderID is a valid objectID and that it exists in our DB
+async function validateOrderID(req, res, next) {
+    if (!ObjectId.isValid(req.params.orderID)) return res.status(400).send({ msg: ['orderID is not a valid ObjectID'] });
+    const orderExists = await Order.exists({ _id: req.params.orderID });
+    if (!orderExists) return res.status(404).send({ msg: ['Order does not exist'] });
+    next();
+}
+
 // Returns true if a given trackdayID is within lockout period. Returns false for days in the past
 async function isInLockoutPeriod(trackdayID) {
     const trackday = await Trackday.findById(trackdayID);
     const timeLockout = process.env.DAYS_LOCKOUT * (1000 * 60 * 60 * 24); // Lockout period in milliseconds
-    const allowance = (14*60 + 1) * 60 * 1000; // 14 hours and 1 min milliseconds
+    const allowance = (14 * 60 + 1) * 60 * 1000; // 14 hours and 1 min milliseconds
     const adjustedLockout = timeLockout - allowance; // Subtract allowance from the lockout period
 
     const timeDifference = trackday.date.getTime() - Date.now();
@@ -127,7 +137,7 @@ async function verifyJWT(req, res, next) {
 
             // Attach newly created accessToken to httpOnly cookie
             res.cookie('JWT_ACCESS_TOKEN', accessToken, {
-                secure: process.env.NODE_ENV === 'production',  
+                secure: process.env.NODE_ENV === 'production',
                 httpOnly: true,
                 sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
                 maxAge: process.env.JWT_ACCESS_TOKEN_EXPIRATION
@@ -199,4 +209,15 @@ async function verifyJWT_LS(req, res, next) {
 
 
 
-module.exports = { validateForm, validateUserID, validateTrackdayID, validateBikeID, validateQRID, validateProductID, isInLockoutPeriod, hasTrackdayWithinLockout, verifyJWT }
+module.exports = {
+    validateForm,
+    validateUserID,
+    validateTrackdayID,
+    validateBikeID,
+    validateQRID,
+    validateProductID,
+    validateOrderID,
+    isInLockoutPeriod,
+    hasTrackdayWithinLockout,
+    verifyJWT
+}
