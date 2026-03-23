5f51c03d2f5285709e809a084c86904ecc63f48e
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-03-23 14:30:31 -0400

    Fixed issue where single member array not being properly read along with a few bug fixes

--- api/mailScheduler.js
+++ api/mailScheduler.js
@@ -18,32 +18,46 @@ async function checkOutgoingMail() {
         );
         if (!mail) break;
         try {
-            let allUsers;
-            const targets = mail.params.target;
+            let allUsers = [];
+            const isBlast = mail.to.length > 1
+            const targets = mail.params?.target;
             // Verify required params for email blast
-            if (mail.to.length > 1) {
+            if (isBlast) {
                 if (!targets) throw new Error('Email blast has undefined targets');
 
                 // Get all users name and email so we can populate the name in the sendEmail param
                 allUsers = await User.find().select('firstName contact.email').exec();
 
-                logger.info({message: `Begin email blast to ${targets} members. ${mail._id}`});
+                logger.info({ message: `Begin email blast to ${targets} members. ${mail._id}` });
             }
 
             // Send email to all recepients
             for (const recipient of mail.to) {
-                let user = allUsers.find(u => u.contact.email === recipient);
-                user = user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1);
+                let params;
+
+                // Build params based on if email blast or not
+                if (isBlast) {
+                    const user = allUsers.find(u => u.contact.email === recipient);
+                    if (!user) {
+                        logger.warn({ message: `User not found for ${recipient}, skipping.` });
+                        continue;
+                    }
+                    const firstName = user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1);
+                    params = { name: firstName };
+                } else {
+                    params = mail.params || {};
+                }
+
 
                 await sendEmail(
                     recipient,
                     mail.subject,
-                    (mail.to.length === 1) ? mailTemplates[mail.message] : mail.message,
-                    (mail.to.length === 1) ? mail.params || {} : { name: user }
+                    isBlast ? mail.message : mailTemplates[mail.message],
+                    params
                 );
                 await sleep(10); // wait 10 seconds before next email
             }
-            if (mail.to.length > 1) logger.info({message: `Finish email blast to ${targets} members. ${mail._id}`});
+            if (isBlast) logger.info({ message: `Finish email blast to ${targets} members. ${mail._id}` });
             // Successfully sent emails, remove from DB
             await ScheduledMail.deleteOne({ _id: mail._id });
         } catch (err) {
