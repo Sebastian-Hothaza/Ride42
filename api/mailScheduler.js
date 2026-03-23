19f9c61c029400cfdc0bb5a2d95b938973b8b8db
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-03-23 15:18:25 -0400

    Refined emailBlast identifying logic

--- api/mailScheduler.js
+++ api/mailScheduler.js
@@ -18,36 +18,34 @@ async function checkOutgoingMail() {
         );
         if (!mail) break;
         try {
-            let allUsers = [];
-            const isBlast = mail.to.length > 1
+
             const targets = mail.params?.target;
-            // Verify required params for email blast
+            const isBlast = !!targets; // blast is defined by having targets
+
+            let allUsers = [];
+
             if (isBlast) {
-                if (!targets) throw new Error('Email blast has undefined targets');
-
-                // Get all users name and email so we can populate the name in the sendEmail param
+                // Get all users so we can populate the name for the sendEmail param
                 allUsers = await User.find().select('firstName contact.email').exec();
-
                 logger.info({ message: `Begin email blast to ${targets} members. ${mail._id}` });
             }
 
-            // Send email to all recepients
+            // Send emails
             for (const recipient of mail.to) {
                 let params;
 
-                // Build params based on if email blast or not
                 if (isBlast) {
-                    const user = allUsers.find(u => u.contact.email === recipient);
+                    const user = allUsers.find(u => u.contact.email.toLowerCase() === recipient.toLowerCase());
                     if (!user) {
                         logger.warn({ message: `User not found for ${recipient}, skipping.` });
                         continue;
                     }
-                    const firstName = user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1);
+                    const firstName = user.firstName ? user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1) : 'there';
                     params = { name: firstName };
                 } else {
+                    // transactional or non-blast email uses mailTemplates
                     params = mail.params || {};
                 }
-
 
                 await sendEmail(
                     recipient,
@@ -55,9 +53,11 @@ async function checkOutgoingMail() {
                     isBlast ? mail.message : mailTemplates[mail.message],
                     params
                 );
-                await sleep(10); // wait 10 seconds before next email
+                await sleep(10); // rate limiting
             }
-            if (isBlast) logger.info({ message: `Finish email blast to ${targets} members. ${mail._id}` });
+            if (isBlast) {
+                logger.info({ message: `Finish email blast to ${targets} members. ${mail._id}` });
+            }
             // Successfully sent emails, remove from DB
             await ScheduledMail.deleteOne({ _id: mail._id });
         } catch (err) {
