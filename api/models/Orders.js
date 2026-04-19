a22006f6bd34e46d603fa0c12187b0e4c3093a48
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-04-19 15:58:46 +0000

    Simplified schema, mongoDB should not be performing validations

--- api/models/Orders.js
+++ api/models/Orders.js
@@ -10,11 +10,12 @@ const OrderSchema = new mongoose.Schema({
     // Snapshot of product customer selected
 
     // For ALL orders
-    name: { type: String, required: true },
-    category: { type: String, required: true, enum: ["tire", "gear"] },
-    size: { type: String, required: true },
-    quantity: { type: Number, required: true, min: 1 },
-    price: { type: Number, required: true },
+    name: String,
+    category: String,
+    size: String,
+    quantity: Number,
+    basePrice: Number,
+    price: Number,
 
     // For Tire
     compound: String,
@@ -25,10 +26,10 @@ const OrderSchema = new mongoose.Schema({
     addOns: { type: [{ name: String, price: Number, quantity: Number, }], required: false, default: undefined },
 
   }],
-  balanceDue: { type: Number, required: true },
+  balanceDue: Number,
   paymentStatus: { type: String, enum: ["pending", "partial", "paid"], default: "pending" },
   orderStatus: { type: String, enum: ["pending", "complete", "pending design", "pending measurements", "pending approval"], default: "pending" },
-  deliveryDate: { type: Date },
+  deliveryDate: Date
 });
 
 module.exports = mongoose.model("Order", OrderSchema);
