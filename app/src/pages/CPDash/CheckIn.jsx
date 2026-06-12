872866d0b123159f44a4db5fe8adac11708df275
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-06-12 16:46:19 +0000

    Added error logging upon API failure

--- app/src/pages/CPDash/CheckIn.jsx
+++ app/src/pages/CPDash/CheckIn.jsx
@@ -84,7 +84,7 @@ const CheckIn = ({ APIServer, allTrackdays }) => {
             }
         } catch (err) {
             setActiveModal({ type: 'failure', msg: 'API Failure' })
-            console.log(err.message)
+            logger.error({ message: err.message })
         }
 
     }
