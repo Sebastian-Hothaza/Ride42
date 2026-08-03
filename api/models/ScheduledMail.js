f627390ea1a8ba97f98dd793fb0ff8f20fccb2cb
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-08-03 18:11:19 +0000

    Added emailType and trackdayId

--- api/models/ScheduledMail.js
+++ api/models/ScheduledMail.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 const scheduledMailSchema = new mongoose.Schema({
     sendOn: { type: Date, required: true, },
-    processing: { type: Boolean, default: false }, // Flag to indicate if the email is being processed
-    to: [{ type: String, required: true }],
+    processing: { type: Boolean, default: false }, // Flag to indicate if the email is being processed,
+    emailType: { type: String, enum: ["transactional", "marketing", "pmtReminder"], default: "transactional" },
+    trackdayId: { type: mongoose.Schema.Types.ObjectId, ref: "Trackday" }, // Optional reference to a Trackday document, if the email is related to a specific trackday  
+    to: [{ type: String, required: true }], // List of recipient email addresses
     subject: { type: String, required: true },
-    message: { type: String, required: true },
-    params: { type: Object }, // Flexible for extra fields such as name, date, etc.
+    message: { type: String, required: true }, // HTML content of the email
+    args: { type: Object }, // Flexible for extra fields such as name, date, etc.
 });
 
 
