c2fd9a148a91dafb32a97a6c6f1e64a9e72462a6
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-08-10 21:59:09 +0000

    Add retry index

--- api/models/ScheduledMail.js
+++ api/models/ScheduledMail.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const scheduledMailSchema = new mongoose.Schema({
     sendOn: { type: Date, required: true, },
     processing: { type: Boolean, default: false }, // Flag to indicate if the email is being processed,
+    nextRecipientIndex: { type: Number, default: 0 }, // Index of the next recipient to send the email to used in case of a crash or restart, so we can resume sending from where we left off
     emailType: { type: String, enum: ["transactional", "marketing", "pmtReminder"], default: "transactional" },
     trackdayId: { type: mongoose.Schema.Types.ObjectId, ref: "Trackday" }, // Optional reference to a Trackday document, if the email is related to a specific trackday  
     to: [{ type: String, required: true }], // List of recipient email addresses
